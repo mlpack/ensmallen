c7ce4ffe755ba94c2c76757104762000311613f8
Ryan Curtin <ryan@ratml.org>
2018-10-05 18:38:34 -0400

    Fix typo.

--- include/ensmallen.hpp
+++ include/ensmallen.hpp
@@ -71,7 +71,7 @@
 #include "ensmallen_bits/sgdr/snapshot_ensembles.hpp"
 #include "ensmallen_bits/sgdr/snapshot_sgdr.hpp"
 #include "ensmallen_bits/smorms3/smorms3.hpp"
-#include "ensmallen_bits/spalera_sgd/spalera_sgdhpp"
+#include "ensmallen_bits/spalera_sgd/spalera_sgd.hpp"
 #include "ensmallen_bits/svrg/svrg.hpp"
 
 #endif
