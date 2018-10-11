ef4fd47a7225060d9e566ba3009f9b0b8d796e26
conrad <conrad@localhost.localdomain>
2018-10-11 16:52:33 +1000

    prevent warnings for omp pragmas

--- include/ensmallen_bits/config.hpp
+++ include/ensmallen_bits/config.hpp
@@ -7,12 +7,20 @@
 
 
 #if !defined(ENS_PRINT_INFO)
-  #define ENS_PRINT_INFO
+  // #define ENS_PRINT_INFO
 #endif
 
 #if !defined(ENS_PRINT_WARN)
-  #define ENS_PRINT_WARN
+  // #define ENS_PRINT_WARN
 #endif
+
+#if defined(ARMA_USE_OPENMP)
+  #undef  ENS_USE_OPENMP
+  #define ENS_USE_OPENMP
+#endif
+
+
+//
 
 
 #if defined(ENS_DONT_PRINT_INFO)
@@ -21,4 +29,20 @@
 
 #if defined(ENS_DONT_PRINT_WARN)
   #undef ENS_PRINT_WARN
+#endif
+
+#if defined(ENS_DONT_USE_OPENMP)
+  #undef ENS_USE_OPENMP
+#endif
+
+
+//
+
+
+#if defined(ENS_USE_OPENMP)
+  #define ENS_PRAGMA_OMP_PARALLEL _Pragma("omp parallel")
+  #define ENS_PRAGMA_OMP_ATOMIC   _Pragma("omp atomic")
+#else
+  #define ENS_PRAGMA_OMP_PARALLEL
+  #define ENS_PRAGMA_OMP_ATOMIC
 #endif
