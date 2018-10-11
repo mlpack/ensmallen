7fe16795c44f0143300164b3a5b66b4a02723115
conrad <conrad@localhost.localdomain>
2018-10-11 17:52:26 +1000

    abort compilation as early as possible when using a prehistoric compiler

--- include/ensmallen.hpp
+++ include/ensmallen.hpp
@@ -15,6 +15,20 @@
 #ifndef ENSMALLEN_HPP
 #define ENSMALLEN_HPP
 
+// certain compilers are way behind the curve
+#if (defined(_MSVC_LANG) && (_MSVC_LANG >= 201402L))
+  #undef  ARMA_USE_CXX11
+  #define ARMA_USE_CXX11
+#endif
+
+#include <armadillo>
+
+#if !defined(ARMA_USE_CXX11)
+  // armadillo automatically enables ARMA_USE_CXX11
+  // when a C++11/C++14/C++17/etc compiler is detected
+  #error "please enable C++11/C++14 mode in your compiler"
+#endif
+
 #include <cmath>
 #include <cstdlib>
 #include <cstdio>
@@ -30,25 +44,11 @@
 #include <string>
 #include <sstream>
 
-// certain compilers are way behind the curve
-#if (defined(_MSVC_LANG) && (_MSVC_LANG >= 201402L))
-  #undef  ARMA_USE_CXX11
-  #define ARMA_USE_CXX11
-#endif
-
 // On Visual Studio, disable C4519 (default arguments for function templates)
 // since it's by default an error, which doesn't even make any sense because
 // it's part of the C++11 standard.
 #ifdef _MSC_VER
   #pragma warning(disable : 4519)
-#endif
-
-#include <armadillo>
-
-#if !defined(ARMA_USE_CXX11)
-  // armadillo automatically enables ARMA_USE_CXX11
-  // when a C++11/C++14/C++17/etc compiler is detected
-  #error "please enable C++11/C++14 mode in your compiler"
 #endif
 
 #include "ensmallen_bits/config.hpp"
