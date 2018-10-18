3104d5bbc419190e1b116b16b584360591f581b4
Marcus Edel <marcus.edel@fu-berlin.de>
2018-10-18 02:48:45 +0200

    Add axis to plot (does this look better) save as eps.

--- doc/mlsys_paper/experiments/plot.py
+++ doc/mlsys_paper/experiments/plot.py
@@ -10,6 +10,9 @@ smorms3 = np.genfromtxt("smorms3.txt")
 
 a = np.linspace(0, 5, 315)
 
+plt.rcParams.update({'font.size': 14})
+plt.rcParams.update({'font.weight': 'light'})
+
 plt.semilogy(a, adam, 'r-')
 plt.semilogy(a, adagrad, 'b-')
 plt.semilogy(a, rmsprop, 'g-')
@@ -18,4 +21,9 @@ plt.semilogy(a, smorms3, 'c-')
 plt.semilogy(a, sgd, 'y-')
 
 plt.legend(["Adam", "AdaGrad", "RMSprop", "SPALeRA", "SMORMS3", "SGD"])
+plt.ylabel("loss")
+plt.xlabel("epochs")
+
+plt.savefig('learning_curves_crop.eps', format='eps', dpi=1000)
+
 plt.show()
