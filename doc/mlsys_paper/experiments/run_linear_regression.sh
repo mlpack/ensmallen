69857d4dcd20fb908bc790817ff0f2fa45155ff1
Ryan Curtin <ryan@ratml.org>
2018-10-17 23:59:59 -0400

    Update scripts to run what's needed for table 3.

--- doc/mlsys_paper/experiments/run_linear_regression.sh
+++ doc/mlsys_paper/experiments/run_linear_regression.sh
@@ -9,14 +9,16 @@ do
   ./linear_regression_two_functions 100 $n | head -1
   echo -n "ensmallen, EvaluateWithGradient(): ";
   ./linear_regression 100 $n | head -1
-  echo -n "julia, default autodiff: "
-  julia linear_regression.jl 100 $n | tail -1
-  echo -n "julia, autodiff package: "
+  #echo -n "julia, default autodiff: "
+  #julia linear_regression.jl 100 $n | tail -1
+  echo -n "julia, forwarddiff package: "
   julia linear_regression_autodiff.jl 100 $n | tail -1
   echo -n "julia, gradient implemented: "
   julia linear_regression_with_gradient.jl 100 $n | tail -1
   echo -n "scipy: "
   python linear_regression.py 100 $n | head -1
+  echo -n "autograd: "
+  python linear_regression_autograd.py 100 $n | head -1
   echo -n "octave: "
   octave-cli linear_regression.m 100 $n | tail -1 | sed 's/Elapsed time is //'
   echo ""
@@ -27,13 +29,15 @@ echo -n "ensmallen: ";
 ./linear_regression_two_functions 1000 100000 | head -1
 echo -n "ensmallen, EvaluateWithGradient(): ";
 ./linear_regression 1000 100000 | head -1
-echo -n "julia, default autodiff: "
-julia linear_regression.jl 1000 100000 | tail -1
-echo -n "julia, autodiff package: "
+#echo -n "julia, default autodiff: "
+#julia linear_regression.jl 1000 100000 | tail -1
+echo -n "julia, forwarddiff package: "
 julia linear_regression_autodiff.jl 1000 100000 | tail -1
 echo -n "julia, gradient implemented: "
 julia linear_regression_with_gradient.jl 1000 100000 | tail -1
 echo -n "scipy: "
 python linear_regression.py 1000 100000 | head -1
+echo -n "autograd: "
+python linear_regression_autograd.py 1000 100000 | head -1
 echo -n "octave: "
 octave-cli linear_regression.m 1000 100000 | tail -1 | sed 's/Elapsed time is //'
