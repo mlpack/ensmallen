32426da95df90a6ba91664f2ca501814372e2998
Ryan Curtin <ryan@ratml.org>
2018-10-18 00:48:42 -0400

    Match version file to style of the rest of ensmallen.
    ('ens::version' seems better than 'ens::ensVersion' to me)

--- include/ensmallen_bits/ens_version.hpp
+++ include/ensmallen_bits/ens_version.hpp
@@ -6,61 +6,35 @@
 // http://www.opensource.org/licenses/BSD-3-Clause
 
 
-// following the Semantic Versioning pattern defined in https://semver.org/
+// This follows the Semantic Versioning pattern defined in https://semver.org/.
 
 #define ENS_VERSION_MAJOR 0
-#define ENS_VERSION_MINOR 100
+// The minor version is two digits so regular numerical comparisons of versions
+// work right.
+#define ENS_VERSION_MINOR 00
+// Release candidates may have patch versions "RC<X>", i.e., RC0, RC1, and so
+// forth.
 #define ENS_VERSION_PATCH 0
 #define ENS_VERSION_NAME  "development"
 
-//#define ENS_VERSION_NAME "2018-10-31"
-//#define ENS_VERSION_NAME "stable"
-//#define ENS_VERSION_NAME "1.199-RC3"
-
-// for normal releases, ENS_VERSION_NAME can be a date, nickname, etc.
-// a release candidate (RC) can follow the versioning pattern of normal releases,
-// but use the patch level to indicate the RC level,
-// and the RC status explicitly denoted via the ENS_VERSION_NAME;
-// for example, version 1.199.3 can be denoted as "1.199-RC3".
-
-// CS note: i'm not a fan of misuse of the decimal point in version names.
-// CS note: for example, using 1.10 to indicate a newer version than 1.9
-// CS note: breaks the usual mathematical meaning of 1.9 > 1.10
-// CS note: this is why armadillo uses "large" minor version numbers
-// CS note: fixed to 3 digits and no leading zeros (eg. 100).
-// CS note: this approach reserves a lot of room for denoting minor
-// CS note: version upgrades (eg. 110, 120, 199, 200, etc)
-// CS note: without resulting in mathematical ugliness.
-// CS note: i suggest using a minor version number that starts at 100
-// CS note: and is not allowed go past 999.
-
 namespace ens {
 
-struct ens_version
-  {
+struct version
+{
   static const unsigned int major = ENS_VERSION_MAJOR;
   static const unsigned int minor = ENS_VERSION_MINOR;
   static const unsigned int patch = ENS_VERSION_PATCH;
-  
-  static
-  inline
-  std::string
-  as_string()
-    {
+
+  static inline std::string as_string()
+  {
     const char* nickname = ENS_VERSION_NAME;
-    
+
     std::stringstream ss;
-    ss << ens_version::major
-       << '.'
-       << ens_version::minor
-       << '.'
-       << ens_version::patch
-       << " ("
-       << nickname
-       << ')';
-    
+    ss << version::major << '.' << version::minor << '.' << version::patch
+       << " (" << nickname << ')';
+
     return ss.str();
-    }
-  };
+  }
+};
 
 } // namespace ens
