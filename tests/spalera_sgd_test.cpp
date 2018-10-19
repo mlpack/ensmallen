aa932b61fa561754660628a3e731236083e23f92
Ryan Curtin <ryan@ratml.org>
2018-10-19 01:06:14 -0400

    Allow a failure in SPALeRA test and retry.

--- tests/spalera_sgd_test.cpp
+++ tests/spalera_sgd_test.cpp
@@ -27,18 +27,30 @@ TEST_CASE("LogisticRegressionTest","[SPALeRASGDTest]")
   // Now run mini-batch SGD with a couple of batch sizes.
   for (size_t batchSize = 30; batchSize < 50; batchSize += 5)
   {
-    SPALeRASGD<> optimizer(0.05 / batchSize, batchSize, 10000, 1e-4);
-    LogisticRegression<> lr(shuffledData, shuffledResponses, 0.5);
+    bool success = false;
 
-    arma::mat coordinates = lr.GetInitialPoint();
-    optimizer.Optimize(lr, coordinates);
+    // It's possible that convergence can randomly fail.  So allow up to three
+    // trials.
+    for (size_t trial = 0; trial < 3; ++trial)
+    {
+      SPALeRASGD<> optimizer(0.05 / batchSize, batchSize, 10000, 1e-4);
+      LogisticRegression<> lr(shuffledData, shuffledResponses, 0.5);
 
-    // Ensure that the error is close to zero.
-    const double acc = lr.ComputeAccuracy(data, responses, coordinates);
-    REQUIRE(acc == Approx(100.0).epsilon(0.015)); // 1.5% error tolerance.
+      arma::mat coordinates = lr.GetInitialPoint();
+      optimizer.Optimize(lr, coordinates);
 
-    const double testAcc = lr.ComputeAccuracy(testData, testResponses,
-        coordinates);
-    REQUIRE(testAcc == Approx(100.0).epsilon(0.024)); // 2.4% error tolerance.
+      // Ensure that the error is close to zero.
+      const double acc = lr.ComputeAccuracy(data, responses, coordinates);
+      const double testAcc = lr.ComputeAccuracy(testData, testResponses,
+          coordinates);
+
+      if (acc >= 98.5 && testAcc >= 97.6)
+      {
+        success = true;
+        break;
+      }
+    }
+
+    REQUIRE(success == true); // At least one trial must succeed.
   }
 }
