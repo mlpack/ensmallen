f6f5f5e710c4fda29a44d4e6beb492f0bd4da2a0
Ryan Curtin <ryan@ratml.org>
2018-10-19 09:31:40 -0400

    Add a simple block for setting random seed.

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,9 +7,18 @@
 
 int main(int argc, char** argv)
 {
+  /**
+   * Uncomment these three lines if you want to test with different random seeds
+   * each run.  This is good for ensuring that a test's tolerance is sufficient
+   * across many different runs.
+   */
+  //size_t seed = std::time(NULL);
+  //srand((unsigned int) seed);
+  //arma::arma_rng::set_seed(seed);
+
   std::cout << "ensmallen version: " << ens::version::as_string() << std::endl;
-  
+
   std::cout << "armadillo version: " << arma::arma_version::as_string() << std::endl;
-  
+
   return Catch::Session().run(argc, argv);
 }
