696bea9189db2c71bdd68cd2ac0bc520b1b80159
Marcus Edel <marcus.edel@fu-berlin.de>
2018-10-28 15:24:28 +0100

    Add WNGrad optimizer.

--- include/ensmallen.hpp
+++ include/ensmallen.hpp
@@ -99,5 +99,6 @@
 #include "ensmallen_bits/smorms3/smorms3.hpp"
 #include "ensmallen_bits/spalera_sgd/spalera_sgd.hpp"
 #include "ensmallen_bits/svrg/svrg.hpp"
+#include "ensmallen_bits/wn_grad/wn_grad.hpp"
 
 #endif
