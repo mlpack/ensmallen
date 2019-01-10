6c1b7e9b5614f54f31117e5d15757791dc8e07c7
rajiv2605 <rajiv.vaidyanathan4@gmail.com>
2019-01-10 18:07:13 +0530

    Updated spsa test files

--- tests/spsa_test.cpp
+++ tests/spsa_test.cpp
@@ -1,105 +1,96 @@
 /**
  * @file spsa_test.cpp
  * @author N Rajiv Vaidyanathan
+ * @author Marcus Edel
  *
- * Test file for SPSA (simultaneous pertubation stochastic approximation).
+ * Test file for the SPSA optimizer.
  *
- * ensmallen is free software; you may redistribute it and/or modify it under the
- * terms of the 3-clause BSD license.  You should have received a copy of the
- * 3-clause BSD license along with ensmallen.  If not, see
+ * ensmallen is free software; you may redistribute it and/or modify it under
+ * the terms of the 3-clause BSD license.  You should have received a copy of
+ * the 3-clause BSD license along with ensmallen.  If not, see
  * http://www.opensource.org/licenses/BSD-3-Clause for more information.
  */
 
 #include <ensmallen.hpp>
-
-#include <boost/test/unit_test.hpp>
+#include "catch.hpp"
 #include "test_function_tools.hpp"
 
-BOOST_AUTO_TEST_SUITE(SPSATest);
+using namespace arma;
+using namespace ens;
+using namespace ens::test;
 
 /**
- * Tests the SPSA optimizer using the Sphere Function.
+ * Test the SPSA optimizer on the SGDTest function.
  */
-BOOST_AUTO_TEST_CASE(SPSASphereFunctionTest)
+TEST_CASE("SPSASimpleSGDTestFunction","[SPSATest]")
 {
-  for (size_t i = 10; i <= 50; i++)
-  {
-    SphereFunction h(i);
+  SGDTestFunction f;
+  SPSA optimizer(0.1, 1, 0.102, 0.16, 0.3, 100000, 0);
 
-    SPSA optimiser(0.1, 0.102, 0.16,
-                   0.3, 100000);
+  arma::mat coordinates = f.GetInitialPoint();
+  coordinates.ones();
+  double result = optimizer.Optimize(f, coordinates);
 
-    arma::mat coordinates = h.GetInitialPoint();
-    double result = optimiser.Optimize(h, coordinates);
-
-    if (i <= 30)
-      BOOST_REQUIRE_CLOSE(result, 0.0, 10000);
-    else if (i <= 34 || i == 36 || i == 39)
-      BOOST_REQUIRE_CLOSE(result, 8e-33, 10000);
-    else
-      BOOST_REQUIRE_CLOSE(result, 3e-32, 100000);
-
-    BOOST_REQUIRE_SMALL(coordinates[0], 1e-3);
-    BOOST_REQUIRE_SMALL(coordinates[1], 1e-7);
-    BOOST_REQUIRE_SMALL(coordinates[2], 1e-7);
-  }
+  REQUIRE(result == Approx(-1.0).epsilon(0.0005));
+  REQUIRE(coordinates[0] == Approx(0.0).margin(1e-3));
+  REQUIRE(coordinates[1] == Approx(0.0).margin(1e-7));
+  REQUIRE(coordinates[2] == Approx(0.0).margin(1e-7));
 }
 
-BOOST_AUTO_TEST_CASE(SPSALogisticRegressionTest)
+/**
+ * Test the SPSA optimizer on the Sphere function.
+ */
+TEST_CASE("SPSASphereFunctionTest", "[SPSATest]")
 {
-  // Generate a two-Gaussian dataset.
-  GaussianDistribution g1(arma::vec("1.0 1.0 1.0"), arma::eye<arma::mat>(3, 3));
-  GaussianDistribution g2(arma::vec("9.0 9.0 9.0"), arma::eye<arma::mat>(3, 3));
+  SphereFunction f(2);
+  SPSA optimizer(0.1, 2, 0.102, 0.16, 0.3, 100000, 0);
 
-  arma::mat data(3, 1000);
-  arma::Row<size_t> responses(1000);
-  for (size_t i = 0; i < 500; ++i)
-  {
-    data.col(i) = g1.Random();
-    responses[i] = 0;
-  }
-  for (size_t i = 500; i < 1000; ++i)
-  {
-    data.col(i) = g2.Random();
-    responses[i] = 1;
-  }
+  arma::mat coordinates = f.GetInitialPoint();
+  optimizer.Optimize(f, coordinates);
 
-  // Shuffle the dataset.
-  arma::uvec indices = arma::shuffle(arma::linspace<arma::uvec>(0,
-      data.n_cols - 1, data.n_cols));
-  arma::mat shuffledData(3, 1000);
-  arma::Row<size_t> shuffledResponses(1000);
-  for (size_t i = 0; i < data.n_cols; ++i)
-  {
-    shuffledData.col(i) = data.col(indices[i]);
-    shuffledResponses[i] = responses[indices[i]];
-  }
+  REQUIRE(coordinates[0] == Approx(0.0).margin(0.1));
+  REQUIRE(coordinates[1] == Approx(0.0).margin(0.1));
+}
 
-  // Create a test set.
-  arma::mat testData(3, 1000);
-  arma::Row<size_t> testResponses(1000);
-  for (size_t i = 0; i < 500; ++i)
-  {
-    testData.col(i) = g1.Random();
-    testResponses[i] = 0;
-  }
-  for (size_t i = 500; i < 1000; ++i)
-  {
-    testData.col(i) = g2.Random();
-    testResponses[i] = 1;
-  }
+/**
+ * Test the SPSA optimizer on the Matyas function.
+ */
+TEST_CASE("SPSAMatyasFunctionTest", "[SPSATest]")
+{
+  MatyasFunction f;
+  SPSA optimizer(0.1, 1, 0.102, 0.16, 0.3, 100000, 0);
 
-  SPSA optimiser(0.1, 0.102, 0.16,
-               0.3, 100000);
+  arma::mat coordinates = f.GetInitialPoint();
+  optimizer.Optimize(f, coordinates);
 
-  LogisticRegression<> lr(shuffledData, shuffledResponses, optimiser, 0.5);
+  // 3% error tolerance.
+  REQUIRE((std::trunc(100.0 * coordinates[0]) / 100.0) ==
+      Approx(0.0).epsilon(0.003));
+  REQUIRE((std::trunc(100.0 * coordinates[1]) / 100.0) ==
+      Approx(0.0).epsilon(0.003));
+}
+
+/**
+ * Run SPSA on logistic regression and make sure the results are acceptable.
+ */
+TEST_CASE("SPSALogisticRegressionTest", "[SPSATest]")
+{
+  arma::mat data, testData, shuffledData;
+  arma::Row<size_t> responses, testResponses, shuffledResponses;
+
+  LogisticRegressionTestData(data, testData, shuffledData,
+      responses, testResponses, shuffledResponses);
+  LogisticRegression<> lr(shuffledData, shuffledResponses, 0.5);
+
+  SPSA optimizer(0.1, 1, 0.102, 0.16, 0.3, 100000, 1e-8);
+  arma::mat coordinates = lr.GetInitialPoint();
+  optimizer.Optimize(lr, coordinates);
 
   // Ensure that the error is close to zero.
-  const double acc = lr.ComputeAccuracy(data, responses);
-  BOOST_REQUIRE_CLOSE(acc, 100.0, 100); // 0.3% error tolerance.
+  const double acc = lr.ComputeAccuracy(data, responses, coordinates);
+  REQUIRE(acc == Approx(100.0).epsilon(0.003)); // 0.3% error tolerance.
 
-  const double testAcc = lr.ComputeAccuracy(testData, testResponses);
-  BOOST_REQUIRE_CLOSE(testAcc, 100.0, 100); // 0.6% error tolerance.
+  const double testAcc = lr.ComputeAccuracy(testData, testResponses,
+      coordinates);
+  REQUIRE(testAcc == Approx(100.0).epsilon(0.006)); // 0.6% error tolerance.
 }
-
-BOOST_AUTO_TEST_SUITE_END();
