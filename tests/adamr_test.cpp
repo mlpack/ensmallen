1b573a64eea9a5ee7c0d8810940a3558aa94487a
Niteya <niteya.56@gmail.com>
2019-02-14 21:29:51 +0530

    Added Tests for AdamR and AdamWR

---
+++ tests/adamr_test.cpp
@@ -0,0 +1,79 @@
+/**
+ * @file adamr_test.cpp
+ * @author Niteya Shah
+ *
+ * ensmallen is free software; you may redistribute it and/or modify it under
+ * the terms of the 3-clause BSD license.  You should have received a copy of
+ * the 3-clause BSD license along with ensmallen.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#include <ensmallen.hpp>
+#include "catch.hpp"
+#include "test_function_tools.hpp"
+
+using namespace ens;
+using namespace ens::test;
+
+/*
+ * The test to check for cyclic consistency has been perfomed in SGDR test
+ *
+ * Run AdamR on logistic regression and make sure the results are acceptable.
+ */
+
+TEST_CASE("AdamRLogisticRegressionTest","[AdamRTest]")
+{
+  arma::mat data, testData, shuffledData;
+  arma::Row<size_t> responses, testResponses, shuffledResponses;
+
+  LogisticRegressionTestData(data, testData, shuffledData,
+      responses, testResponses, shuffledResponses);
+
+  // Now run AdamR with a couple of batch sizes.
+  for (size_t batchSize = 5; batchSize < 50; batchSize += 5)
+  {
+    AdamR adamr(0.01, 50, 2.0, batchSize, 0.9, 0.999 ,1e-8, 10000, 1e-3);
+    LogisticRegression<> lr(shuffledData, shuffledResponses, 0.5);
+
+    arma::mat coordinates = lr.GetInitialPoint();
+    adamr.Optimize(lr, coordinates);
+
+    // Ensure that the error is close to zero.
+    const double acc = lr.ComputeAccuracy(data, responses, coordinates);
+    REQUIRE(acc == Approx(100.0).epsilon(0.003)); // 0.3% error tolerance.
+
+    const double testAcc = lr.ComputeAccuracy(testData, testResponses,
+        coordinates);
+    REQUIRE(testAcc == Approx(100.0).epsilon(0.006)); // 0.6% error tolerance.
+  }
+}
+
+/**
+ * Run AdamWR on logistic regression and make sure the results are better.
+ */
+TEST_CASE("AdamWRLogisticRegressionTest","[AdamRTest]")
+{
+  arma::mat data, testData, shuffledData;
+  arma::Row<size_t> responses, testResponses, shuffledResponses;
+
+  LogisticRegressionTestData(data, testData, shuffledData,
+      responses, testResponses, shuffledResponses);
+
+  // Now run AdamWR with a couple of batch sizes.
+  for (size_t batchSize = 5; batchSize < 50; batchSize += 5)
+  {
+    AdamWR adamwr(0.03, 100, 2.0, batchSize, 0.9, 0.999 ,1e-8, 10000, 1e-3);
+    LogisticRegression<> lr(shuffledData, shuffledResponses, 0.5);
+
+    arma::mat coordinates = lr.GetInitialPoint();
+    adamwr.Optimize(lr, coordinates);
+
+    // Ensure that the error is close to zero.
+    const double acc = lr.ComputeAccuracy(data, responses, coordinates);
+    REQUIRE(acc == Approx(100.0).epsilon(0.001)); // 0.1% error tolerance.
+
+    const double testAcc = lr.ComputeAccuracy(testData, testResponses,
+        coordinates);
+    REQUIRE(testAcc == Approx(100.0).epsilon(0.003)); // 0.3% error tolerance.
+  }
+}
