2457a7797680ca59a8a8bdeb0d4cb1d1b185cb2f
Suryoday Basak <suryodaybasak@gmail.com>
2019-02-16 07:32:37 -0600

    Adding an implementation of PSO

---
+++ include/ensmallen_bits/pso/pso.hpp
@@ -0,0 +1,137 @@
+/**
+ * @file pso.hpp
+ * @author Chintan Soni
+ * @author Suryoday Basak
+ *
+ * Particle swarm optimization.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef ENSMALLEN_PSO_PSO_HPP
+#define ENSMALLEN_PSO_PSO_HPP
+
+#include "update_policies/lbest_update.hpp"
+#include "init_policies/default_init.hpp"
+#include <iostream>
+
+namespace ens {
+
+/**
+ * EXTREMELY DETAILED DESCRIPTION OF THE WORKING OF PSO.
+ *
+ * For Particle Swarm Optimization to work, a FunctionType template parameter is
+ * required.
+ * This class must implement the following function:
+ *
+ *   double Evaluate(const arma::mat& coordinates);
+ *   void Gradient(const arma::mat& coordinates,
+ *                 arma::mat& gradient);
+ */
+template<typename VelocityUpdatePolicy = LBestUpdate,
+         typename InitPolicy = DefaultInit>
+class PSOType
+{
+ public:
+  /**
+   * Construct the particle swarm optimizer with the given function and
+   * parameters. The defaults here are not necessarily good for the given
+   * problem, so it is suggested that the values used be tailored to the task
+   * at hand.
+   *
+   * @param numParticles Number of particles in the swarm.
+   * @param maxIterations Number of iterations allowed.
+   * @param exploitationFactor Influence of the personal best of the particle.
+   * @param explorationFactor Influence of the neighbours of the particle.
+   * @param enableGradientDescent Enable the use of gradient descent optimizer.
+   * @param psoIterationsRatio Portion of maxIterations which will be run using
+   *    the PSO optimizer. The rest of the iterations will use gradient descent.
+   * @param stepSize The step size for the gradient descent optimizer.
+   */
+  PSOType(
+    const size_t numParticles = 16,
+    const size_t maxIterations = 3000,
+    const double exploitationFactor = 2.05,
+    const double explorationFactor = 2.05,
+    const VelocityUpdatePolicy& velocityUpdatePolicy = VelocityUpdatePolicy(),
+    const InitPolicy& initPolicy = InitPolicy()) :
+    numParticles(numParticles),
+    maxIterations(maxIterations),
+    exploitationFactor(exploitationFactor),
+    explorationFactor(explorationFactor),
+    velocityUpdatePolicy(velocityUpdatePolicy),
+    initPolicy(initPolicy) { /* Nothing to do */ }
+
+  /**
+   * ADD PSO OPTIMIZATION DESCRIPTION.
+   *
+   * Optimize the given function using particle swarm optimization. The given
+   * starting point will be modified to store the finishing point of the
+   * algorithm, and the final objective value is returned.
+   *
+   * @tparam FunctionType Type of the function to optimize.
+   * @param function Function to optimize.
+   * @param iterate Initial point (will be modified).
+   * @return Objective value of the final point.
+   */
+  template<typename FunctionType>
+  double Optimize(FunctionType& function, arma::mat& iterate);
+
+  //! Retrieve value of numParticles.
+  size_t NumParticles() const { return numParticles; }
+
+  //! Modify value of numParticles.
+  size_t& NumParticles() { return numParticles; }
+
+  //! Retrieve value of maxIterations.
+  size_t MaxIterations() const { return maxIterations; }
+
+  //! Modify value of maxIterations.
+  size_t& MaxIterations() { return maxIterations; }
+
+  //! Retrieve value of exploitationFactor.
+  double ExploitationFactor() const { return exploitationFactor; }
+
+  //! Modify value of exploitationFactor.
+  double& ExploitationFactor() { return exploitationFactor; }
+
+  //! Retrieve value of explorationFactor.
+  double ExplorationFactor() const { return explorationFactor; }
+
+  //! Modify value of explorationFactor.
+  double& ExplorationFactor() { return explorationFactor; }
+
+ private:
+  //! Number of particles in the swarm.
+  size_t numParticles;
+  //! Maximum number of iterations for which the optimizer will run.
+  size_t maxIterations;
+  //! Exploitation factor for lbest version.
+  double exploitationFactor;
+  //! Exploration factor for lbest version.
+  double explorationFactor;
+  //! Particle positions.
+  arma::cube particlePositions;
+  //! Particle velocities.
+  arma::cube particleVelocities;
+  //! Particle fitness values.
+  arma::vec particleFitnesses;
+  //! Best fitness attained by particle so far.
+  arma::vec particleBestFitnesses;
+  //! Position corresponding to the best fitness of particle.
+  arma::cube particleBestPositions;
+  //! Velocity update policy used.
+  VelocityUpdatePolicy velocityUpdatePolicy;
+  //! Particle initialization policy used.
+  InitPolicy initPolicy;
+};
+
+using LBestPSO = PSOType<LBestUpdate>;
+
+} // ens
+
+#include "pso_impl.hpp"
+
+#endif
---
+++ include/ensmallen_bits/pso/update_policies/lbest_update.hpp
@@ -0,0 +1,132 @@
+/**
+ * @file lbest_update.hpp
+ * @author Chintan Soni
+ *
+ * The local best version of PSO in which particles communicate with only two
+ * neighbours each, thus forming a ring topology amongst them. This approach
+ * allows PSO to converge at the global minimum, but takes significantly more
+ * iterations to do so.
+ *
+ * mlpack is free software; you may redistribute it and/or modify it under the
+ * terms of the 3-clause BSD license.  You should have received a copy of the
+ * 3-clause BSD license along with mlpack.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+#ifndef ENSMALLEN_PSO_UPDATE_POLICIES_LBEST_UPDATE_HPP
+#define ENSMALLEN_PSO_UPDATE_POLICIES_LBEST_UPDATE_HPP
+#include <assert.h>
+
+namespace ens {
+
+/**
+ * ADD SLIGHTLY MORE DETAILED DESCRIPTION HERE.
+ *
+ * For more information, see the following.
+ *
+ * REFERENCE TO A RELEVANT ARTICLE.
+ *
+ */
+class LBestUpdate
+{
+ public:
+  /**
+   * The Initialize method is called by PSO Optimizer method before the start of
+   * the iteration process. It calculates the value of the constriction
+   * coefficent, initializes the local best indices of each particle to itself,
+   * and sets the shape of the r1 and r2 vectors.
+   *
+   * @param exploitationFactor Influence of personal best achieved.
+   * @param explorationFactor Influence of neighbouring particles.
+   * @param numParticles The number of particles in the swarm.
+   * @param iterate The user input, used for shaping intermediate vectors.
+   */
+  void Initialize(const double& exploitationFactor,
+                  const double& explorationFactor,
+                  const size_t& numParticles,
+                  const arma::mat& iterate)
+  {
+    // Set number of particles.
+    n = numParticles;
+    // Set c1 = exploitationFactor and c2 = explorationFactor.
+    c1 = exploitationFactor;
+    c2 = explorationFactor;
+
+    // Calculate the constriction factor
+    double phi = c1 + c2;
+    assert(phi > 4.0);
+    chi = 2.0 / std::abs(2.0 - phi - std::sqrt((phi - 4.0) * phi));
+
+    // Initialize local best indices to self indices of particles.
+    size_t index = 0;
+    localBestIndices.set_size(n);
+    localBestIndices.imbue([&]() { return index++; });
+
+    // Set sizes r1 and r2.
+    r1.set_size(iterate.n_rows, iterate.n_cols);
+    r2.set_size(iterate.n_rows, iterate.n_cols);
+  }
+
+  /**
+   * Update step for LBestPSO. Compares personal best of each particle with that
+   * of its neighbours, and sets the best of the 3 as the lobal best. This
+   * particle is then used for calculating the velocity for the update step.
+   *
+   * @param particlePositions The current coordinates of particles.
+   * @param particleVelocities The current velocities (will be modified).
+   * @param particleFitnesses The current fitness values or particles.
+   * @param particleBestPositions The personal best coordinates of particles.
+   * @param particleBestFitnesses The personal best fitness values of particles.
+   */
+  void Update(const arma::cube& particlePositions,
+              arma::cube& particleVelocities,
+              const arma::cube& particleBestPositions,
+              const arma::vec& particleBestFitnesses)
+  {
+    // Velocity update logic.
+    for (size_t i = 0; i < n; i++)
+    {
+      localBestIndices(i) =
+        particleBestFitnesses(left(i)) < particleBestFitnesses(i) ?
+        left(i) : i;
+      localBestIndices(i) =
+        particleBestFitnesses(right(i)) < particleBestFitnesses(i) ?
+        right(i) : i;
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+      // Generate random numbers for current particle.
+      r1.randu();
+      r2.randu();
+      particleVelocities.slice(i) = chi * (particleVelocities.slice(i) +
+          c1 * r1 %
+              (particleBestPositions.slice(i) - particlePositions.slice(i)) +
+          c2 * r2 %
+              (particleBestPositions.slice(localBestIndices(i)) -
+                  particlePositions.slice(i)));
+    }
+  }
+
+ private:
+  //! Number of particles.
+  size_t n;
+  //! Exploitation factor.
+  double c1;
+  //! Exploration factor.
+  double c2;
+  //! Constriction factor chi.
+  double chi;
+  //! Vectors of random numbers.
+  arma::mat r1;
+  arma::mat r2;
+  //! Indices of each particle's best neighbour.
+  arma::vec localBestIndices;
+
+  // Helper functions for calculating neighbours.
+  inline size_t left(const size_t index) { return (index + n - 1) % n; }
+  inline size_t right(const size_t index) { return (index + 1) % n; }
+};
+
+} // ens
+
+#endif
