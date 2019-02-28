d8b81a3bede9117fc548747c874f3f4265f9fded
Suryoday Basak <suryodaybasak@gmail.com>
2019-02-28 01:33:21 -0600

    Fixing test case names in pso_test

--- tests/pso_test.cpp
+++ tests/pso_test.cpp
@@ -12,59 +12,41 @@
  */
 #include <ensmallen.hpp>
 #include "catch.hpp"
-#include "test_function_tools.hpp"
 
 using namespace ens;
 using namespace ens::test;
 
-/**
- * Tests the LBest optimizer using simple test functions.
- */
-
-TEST_CASE("RastriginFunctionTest", "[PSOTest]")
-{
-  RastriginFunction f(4);
-  LBestPSO s;
-
-  arma::vec coordinates = f.GetInitialPoint();
-  double result = s.Optimize(f, coordinates);
-
-  //REQUIRE(coordinates[j] == Approx(0.0).margin(0.003)); REFERENCE
-  //BOOST_REQUIRE_SMALL(result, 1e-5);
-  REQUIRE(result <= 1e-5); //work on this
-  for (size_t j = 0; j < 4; ++j)
-	REQUIRE(coordinates[j] <= 1e-3); //work on this
-    //BOOST_REQUIRE_SMALL(coordinates[j], 1e-3);
-}
-
-TEST_CASE("RosenbrockFunctionTest", "[PSOTest]")
+TEST_CASE("LBestPSORosenbrockFunctionTest", "[PSOTest]")
 {
   RosenbrockFunction f;
   LBestPSO s;
 
-  arma::vec coordinates = f.GetInitialPoint();
-  double result = s.Optimize(f, coordinates);
+  arma::vec coords = f.GetInitialPoint();
+  if (!s.Optimize(f, coords))
+    FAIL("LBest PSO optimization reported failure for Rosenbrock Function.");
 
-  //BOOST_REQUIRE_SMALL(result, 1e-5);
-  REQUIRE(result <= 1e-5);
-  for (size_t j = 0; j < 2; ++j)
-	REQUIRE(coordinates[j] == Approx(1.0).margin(0.0001)); //work on this
-    //BOOST_REQUIRE_CLOSE(coordinates[j], (double) 1.0, 1e-3);
+  double finalValue = f.Evaluate(coords);
+
+  REQUIRE(finalValue == Approx(0.0).margin(1e-5));
+  REQUIRE(coords[0] == Approx(1.0).epsilon(1e-7));
+  REQUIRE(coords[1] == Approx(1.0).epsilon(1e-7));
 }
 
-TEST_CASE("SphereFunctionTest", "[PSOTest]")
+TEST_CASE("LBestPSOSphereFunctionTest", "[PSOTest]")
 {
   SphereFunction f(4);
   LBestPSO s;
 
-  arma::vec coordinates = f.GetInitialPoint();
-  double result = s.Optimize(f, coordinates);
-
+  arma::vec coords = f.GetInitialPoint();
+  if (!s.Optimize(f, coords))
+    FAIL("LBest PSO optimization reported failure for Sphere Function.");
+  
+  double finalValue = f.Evaluate(coords);
   //BOOST_REQUIRE_SMALL(result, 1e-5);
-  REQUIRE(result <= 1e-5);
+  REQUIRE(finalValue <= 1e-5);
   for (size_t j = 0; j < 4; ++j)
     //BOOST_REQUIRE_SMALL(coordinates[j], 1e-3);
-	REQUIRE(coordinates[j] <= 1e-3); //work on this
+	REQUIRE(coords[j] <= 1e-3); //work on this
 }
 
 //BOOST_AUTO_TEST_SUITE_END();
