83e54b35529b02eeda402ff0dee2bd14eb8b9c13
Ryan Curtin <ryan@ratml.org>
2019-03-09 17:02:30 -0500

    Update and release version 1.14.1.

--- include/ensmallen_bits/ens_version.hpp
+++ include/ensmallen_bits/ens_version.hpp
@@ -16,7 +16,7 @@
 // The minor version is two digits so regular numerical comparisons of versions
 // work right.  The first minor version of a release is always 10.
 #define ENS_VERSION_MINOR 14
-#define ENS_VERSION_PATCH 0
+#define ENS_VERSION_PATCH 1
 // If this is a release candidate, it will be reflected in the version name
 // (i.e. the version name will be "RC1", "RC2", etc.).  Otherwise the version
 // name will typically be a seemingly arbitrary set of words that does not
