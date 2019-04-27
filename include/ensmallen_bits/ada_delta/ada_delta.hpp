a698919e0fc67760c34015a1243f627dd4ccfb83
walragatver <toshalagrawal38@gmail.com>
2019-04-27 15:58:02 +0530

    Mofify ada_delta, ada_grad, ftml, padam optimizers.

--- include/ensmallen_bits/ada_delta/ada_delta.hpp
+++ include/ensmallen_bits/ada_delta/ada_delta.hpp
@@ -64,6 +64,9 @@ class AdaDelta
    * @param tolerance Maximum absolute tolerance to terminate algorithm.
    * @param shuffle If true, the function order is shuffled; otherwise, each
    *        function is visited in linear order.
+   * @param exactObjective Flag that determines whether actual objective over
+   *                       entire training set is calculated or not after
+   *                       training.
    * @param resetPolicy If true, parameters are reset before every Optimize
    *        call; otherwise, their values are retained.
    */
@@ -74,6 +77,7 @@ class AdaDelta
            const size_t maxIterations = 100000,
            const double tolerance = 1e-5,
            const bool shuffle = true,
+           const bool exactObjective = false,
            const bool resetPolicy = true);
 
   /**
@@ -127,6 +131,11 @@ class AdaDelta
   bool Shuffle() const { return optimizer.Shuffle(); }
   //! Modify whether or not the individual functions are shuffled.
   bool& Shuffle() { return optimizer.Shuffle(); }
+
+  //! Get whether or not the actual objective is calculated after training.
+  bool ExactObjective() const { return optimizer.ExactObjective(); }
+  //! Modify whether or not the actual objective is calculated after training.
+  bool& ExactObjective() { return optimizer.ExactObjective(); }
 
   //! Get whether or not the update policy parameters
   //! are reset before Optimize call.
