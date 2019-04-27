048cdba0281541759252e59b6a5e716bbd53c2bb
walragatver <toshalagrawal38@gmail.com>
2019-04-27 16:29:50 +0530

    Modify Adam, momentum, nestrov_momentum optimizer.

--- include/ensmallen_bits/adam/adam.hpp
+++ include/ensmallen_bits/adam/adam.hpp
@@ -86,6 +86,9 @@ class AdamType
    * @param tolerance Maximum absolute tolerance to terminate algorithm.
    * @param shuffle If true, the function order is shuffled; otherwise, each
    *        function is visited in linear order.
+   * @param exactObjective Flag that determines whether actual objective over
+   *                       entire training set is calculated or not after
+   *                       training.
    * @param resetPolicy If true, parameters are reset before every Optimize
    *        call; otherwise, their values are retained.
    */
@@ -97,6 +100,7 @@ class AdamType
            const size_t maxIterations = 100000,
            const double tolerance = 1e-5,
            const bool shuffle = true,
+           const bool exactObjective = false,
            const bool resetPolicy = true);
 
   /**
@@ -154,6 +158,11 @@ class AdamType
   bool Shuffle() const { return optimizer.Shuffle(); }
   //! Modify whether or not the individual functions are shuffled.
   bool& Shuffle() { return optimizer.Shuffle(); }
+
+  //! Get whether or not the actual objective is calculated after training.
+  bool ExactObjective() const { return optimizer.ExactObjective(); }
+  //! Modify whether or not the actual objective is calculated after training.
+  bool& ExactObjective() { return optimizer.ExactObjective(); }
 
   //! Get whether or not the update policy parameters
   //! are reset before Optimize call.
