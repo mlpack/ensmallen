21b8152d64b718edc5386cdd6cada3e8957da432
Suryoday Basak <suryodaybasak@gmail.com>
2019-06-03 16:11:34 -0500

    Fixing code style of ackley_function

--- include/ensmallen_bits/problems/ackley_function_impl.hpp
+++ include/ensmallen_bits/problems/ackley_function_impl.hpp
@@ -19,9 +19,9 @@ using namespace std;
 namespace ens {
 namespace test {
 
-inline AckleyFunction::AckleyFunction(const double trig_coeff,
-				      const double epsilon) : trig_coeff(trig_coeff),
-							      epsilon(epsilon)
+inline AckleyFunction::AckleyFunction(const double c,
+                                      const double epsilon) : c(c),
+                                                              epsilon(epsilon)
 { /* Nothing to do here */}
 
 inline void AckleyFunction::Shuffle() { /* Nothing to do here */ }
@@ -34,9 +34,9 @@ inline double AckleyFunction::Evaluate(const arma::mat& coordinates,
   const double x1 = coordinates(0);
   const double x2 = coordinates(1);
 
-  const double objective = -20 * exp(-0.2 * sqrt(0.5 * (pow(x1, 2) +  pow(x2, 2)))) - 
-	  			 exp(0.5 * (cos(2 * arma::datum::pi * x1) + 
-				 cos(2 * trig_coeff))) + exp(1) + 20;
+  const double objective = -20 * exp(-0.2 * sqrt(0.5 * (x1 * x1 + x2 * x2))) -
+                            exp(0.5 * (cos(c * x1) + cos(c * x2))) + exp(1) +
+                            20;
 
   return objective;
 }
@@ -55,15 +55,15 @@ inline void AckleyFunction::Gradient(const arma::mat& coordinates,
   const double x1 = coordinates(0);
   const double x2 = coordinates(1);
 
-  //Aliases for different terms in the expression of the gradient
-  const double t0 = sqrt(0.5 * (pow(x1, 2) + pow(x2, 2)));
+  // Aliases for different terms in the expression of the gradient.
+  const double t0 = sqrt(0.5 * (x1 * x1 + x2 * x2));
   const double t1 = 2.0 * exp(- 0.2 * t0) / (t0 + epsilon);
-  const double t2 = 0.5 * trig_coeff * exp(0.5 * (cos(trig_coeff * x1) +
-		    cos(trig_coeff * x2)));
+  const double t2 = 0.5 * c * exp(0.5 * (cos(c * x1) +
+		    cos(c * x2)));
 
   gradient.set_size(2, 1);
-  gradient(0) = (x1 * t1) + (t2 * std::sin(trig_coeff * x1));
-  gradient(1) = (x2 * t1) + (t2 * std::sin(trig_coeff * x2));
+  gradient(0) = (x1 * t1) + (t2 * sin(c * x1));
+  gradient(1) = (x2 * t1) + (t2 * sin(c * x2));
 }
 
 inline void AckleyFunction::Gradient(const arma::mat& coordinates,
