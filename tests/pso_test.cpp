e701fc4bda0394e727f839280cefd554609c00e0
Suryoday Basak <suryodaybasak@gmail.com>
2019-07-02 22:06:46 -0500

    Style fixes for lbest_update

--- tests/pso_test.cpp
+++ tests/pso_test.cpp
@@ -16,22 +16,6 @@
 using namespace ens;
 using namespace ens::test;
 
-TEST_CASE("LBestPSORosenbrockFunctionTest", "[PSOTest]")
-{
-  RosenbrockFunction f;
-  LBestPSO s;
-
-  arma::vec coords = f.GetInitialPoint();
-  if (!s.Optimize(f, coords))
-    FAIL("LBest PSO optimization reported failure for Rosenbrock Function.");
-
-  double finalValue = f.Evaluate(coords);
-
-  REQUIRE(finalValue == Approx(0.0).margin(1e-5));
-  REQUIRE(coords[0] == Approx(1.0).epsilon(1e-7));
-  REQUIRE(coords[1] == Approx(1.0).epsilon(1e-7));
-}
-
 TEST_CASE("LBestPSOSphereFunctionTest", "[PSOTest]")
 {
   SphereFunction f(4);
@@ -49,63 +33,16 @@ TEST_CASE("LBestPSOSphereFunctionTest", "[PSOTest]")
 	REQUIRE(coords[j] <= 1e-3); //work on this
 }
 
-
-TEST_CASE("ConstrainedSquaredFunctionTest", "[PSOTest]")
+TEST_CASE("LBestPSORosenbrockTest","[PSOTest]")
 {
-  class SquaredFunction
-  {
-    public:
-    // This returns f(x) = 2 |x|^2.
-    double Evaluate(const arma::mat& x)
-    {
-      if (x.at(0,0) < 0.5)
-	return std::numeric_limits<double>::max();
-      else
-        return std::pow(arma::norm(x), 2.0);
-    }
-  };
-  arma::mat x("2.0");
-
+  RosenbrockFunction f;
   LBestPSO s;
-  SquaredFunction f;
+  arma::vec coordinates = f.GetInitialPoint();
 
-  arma::vec coords = x;
-  if (!s.Optimize(f, coords))
-    FAIL("LBest PSO optimization reported failure for Linear Constrained Square Function.");
-  
-  double finalValue = f.Evaluate(coords);
-  //BOOST_REQUIRE_SMALL(result, 1e-5);
-  REQUIRE(finalValue == Approx(0.25).epsilon(1e-5));
-  REQUIRE(coords[0] == Approx(0.5).epsilon(1e-5));
+  const double result = s.Optimize(f, coordinates);
+
+  REQUIRE(result == Approx(0.0).margin(1e-5));
+  REQUIRE(coordinates[0] == Approx(1.0).epsilon(1e-4));
+  REQUIRE(coordinates[1] == Approx(1.0).epsilon(1e-4));
 }
 
-TEST_CASE("ConstrainedAffineFunctionTest", "[PSOTest]")
-{
-  class AffineFunction
-  {
-    public:
-    // This returns f(x) = 2 |x|^2.
-    double Evaluate(const arma::mat& x)
-    {
-      if (x.at(0,0) < -3.0)
-	return std::numeric_limits<double>::max();
-      else
-        return x.at(0,0);
-    }
-  };
-  arma::mat x("2.0");
-
-  LBestPSO s;
-  AffineFunction f;
-
-  arma::vec coords = x;
-  if (!s.Optimize(f, coords))
-    FAIL("LBest PSO optimization reported failure for Linear Constrained Affine Function.");
-  
-  double finalValue = f.Evaluate(coords);
-  //BOOST_REQUIRE_SMALL(result, 1e-5);
-  REQUIRE(finalValue == Approx(-3.0).epsilon(1e-5));
-  REQUIRE(coords[0] == Approx(-3.0).epsilon(1e-5));
-}
-
-//BOOST_AUTO_TEST_SUITE_END();
