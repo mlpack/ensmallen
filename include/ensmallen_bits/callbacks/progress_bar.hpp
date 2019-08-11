467b7c5718c22dae8879dc1d866548f92d25c7cc
Marcus Edel <marcus.edel@fu-berlin.de>
2019-08-11 22:50:43 +0200

    Per epoch progress bar.

--- include/ensmallen_bits/callbacks/progress_bar.hpp
+++ include/ensmallen_bits/callbacks/progress_bar.hpp
@@ -25,30 +25,143 @@ class ProgressBar
    * Set up the progress bar callback class with the given width and output
    * stream.
    *
-   * @param width Width of the bar.
+   * @param widthIn Width of the bar.
    * @param ostream Ostream which receives output from this object.
    */
-  ProgressBar(const size_t width = 70,
+  ProgressBar(const size_t widthIn = 70,
               std::ostream& output = arma::get_cout_stream()) :
-      step(100.0 / width),
+      width(100.0 / widthIn),
       output(output),
-      previousProgress(0)
+      objective(0),
+      epochs(0),
+      epochSize(0),
+      step(1),
+      steps(0),
+      newEpoch(false),
+      epoch(1)
+
   { /* Nothing to do here. */ }
 
   /**
-   * Callback function called at the start of the optimization process.
+   * Callback function called at the begin of the optimization process.
    *
    * @param optimizer The optimizer used to update the function.
    * @param function Function to optimize.
    * @param coordinates Starting point.
    */
   template<typename OptimizerType, typename FunctionType, typename MatType>
-  void BeginOptimization(OptimizerType& /* optimizer */,
-                         FunctionType& /* function */,
+  void BeginOptimization(OptimizerType& optimizer,
+                         FunctionType& function,
                          MatType& /* coordinates */)
   {
+    epochSize = function.NumFunctions() / optimizer.BatchSize();
+    if (function.NumFunctions() % optimizer.BatchSize() > 0)
+      epochSize++;
+
+    if (!optimizer.MaxIterations())
+    {
+      Warn << "Maximum number of iterations not defined (no limit),"
+           << " no progress bar shown." << std::endl;
+    }
+    else
+    {
+      epochs = optimizer.MaxIterations() / epochSize;
+      if (optimizer.MaxIterations() % epochSize > 0)
+        epochs++;
+    }
+
+    stepTimer.tic();
+  }
+
+  /**
+   * Callback function called at the begin of a pass over the data.
+   *
+   * @param optimizer The optimizer used to update the function.
+   * @param function Function to optimize.
+   * @param coordinates Starting point.
+   * @param epochIn The index of the current epoch.
+   * @param objective Objective value of the current point.
+   */
+  template<typename OptimizerType, typename FunctionType, typename MatType>
+  void BeginEpoch(OptimizerType& /* optimizer */,
+                  FunctionType& /* function */,
+                  const MatType& /* coordinates */,
+                  const size_t epochIn,
+                  const double /* objective */)
+  {
     // Start the timer.
-    timer.tic();
+    epochTimer.tic();
+
+    // Reset epoch parameter.
+    objective = 0;
+    step = 1;
+
+    epoch = epochIn;
+    newEpoch = true;
+  }
+
+  /**
+   * Callback function called once a step is taken.
+   *
+   * @param optimizer The optimizer used to update the function.
+   * @param function Function to optimize.
+   * @param coordinates Starting point.
+   * @param objective Objective value of the current point.
+   */
+  template<typename OptimizerType, typename FunctionType, typename MatType>
+  void StepTaken(OptimizerType& /* optimizer */,
+                 FunctionType& /* function */,
+                 const MatType& /* coordinates */)
+  {
+    if (newEpoch)
+    {
+      output << "Epoch " << epoch << "/" << epochs << "\n";
+      output.flush();
+      newEpoch = false;
+    }
+
+    const size_t progress = ((double) step / epochSize) * 100;
+    output << step++ << "/" << epochSize << " [";
+    for (size_t i = 0; i < 100; i += width)
+    {
+      if (i < progress)
+      {
+        output << "=";
+      }
+      else if (i == progress)
+      {
+        output << ">";
+      }
+      else
+      {
+        output << ".";
+      }
+    }
+
+    output << "] " << progress << "% - ETA: " << (size_t) stepTimer.toc() *
+        (epochSize - step + 1) % 60 << "s - loss: " <<
+        objective / (double) step <<  "\r";
+    output.flush();
+
+    stepTimer.tic();
+  }
+
+  /**
+   * Callback function called at any call to Evaluate().
+   *
+   * @param optimizer The optimizer used to update the function.
+   * @param function Function to optimize.
+   * @param coordinates Starting point.
+   * @param objectiveIn Objective value of the current point.
+   */
+  template<typename OptimizerType, typename FunctionType, typename MatType>
+  void Evaluate(OptimizerType& optimizer,
+                FunctionType& /* function */,
+                const MatType& /* coordinates */,
+                const double objectiveIn)
+  {
+    objective += objectiveIn / optimizer.BatchSize();
+    steps++;
   }
 
   /**
@@ -61,27 +174,15 @@ class ProgressBar
    * @param objective Objective value of the current point.
    */
   template<typename OptimizerType, typename FunctionType, typename MatType>
-  void EndEpoch(OptimizerType& optimizer,
+  void EndEpoch(OptimizerType& /* optimizer */,
                 FunctionType& /* function */,
                 const MatType& /* coordinates */,
-                const size_t epoch,
+                const size_t /* epoch */,
                 const double objective)
   {
-    if (!optimizer.MaxIterations())
-    {
-      Warn << "Maximum number of iterations not defined (no limit),"
-           << " no progress bar shown." << std::endl;
-      return;
-    }
-
-    const size_t progress = ((double) epoch / optimizer.MaxIterations()) * 100;
-
-    // Skip if the progress hasn't changed.
-    if (progress <= previousProgress)
-      return;
-
-    output << epoch << "/" << optimizer.MaxIterations() << " [";
-    for (size_t i = 0; i < 100; i += step)
+    const size_t progress = ((double) (step - 1) / epochSize) * 100;
+    output << step - 1 << "/" << epochSize << " [";
+    for (size_t i = 0; i < 100; i += width)
     {
       if (i < progress)
       {
@@ -93,29 +194,49 @@ class ProgressBar
       }
       else
       {
-        output << " ";
+        output << ".";
       }
     }
 
-    output << "] " << progress << "% - ETA: " << (size_t) timer.toc()
-        << "s - loss: " << objective <<  "\r";
+    const size_t stepTime = epochTimer.toc() / (double) epochSize * 1000;
+    output << "] " << progress << "% - " << (size_t) epochTimer.toc() % 60
+        << "s " << stepTime << "ms/step " << "- loss: " << objective  <<  "\n";
     output.flush();
-
-    previousProgress = progress;
   }
 
  private:
   //! Length of a single step (1%).
-  double step;
+  double width;
 
   //! The output stream that all data is to be sent to; example: std::cout.
   std::ostream& output;
 
-  //! Locally-stored previous progress.
-  size_t previousProgress;
+  //! Objective over the current epoch.
+  double objective;
 
-  //! Locally-stored timer object.
-  arma::wall_clock timer;
+  //! Total number of epochs
+  size_t epochs;
+
+  //! Number of steps per epoch.
+  size_t epochSize;
+
+  //! Current step number.
+  size_t step;
+
+  //! Number of steps taken.
+  size_t steps;
+
+  //! Indicates a new epoch.
+  bool newEpoch;
+
+  //! Locally-stored epoch.
+  size_t epoch;
+
+  //! Locally-stored step timer object.
+  arma::wall_clock stepTimer;
+
+  //! Locally-stored epoch timer object.
+  arma::wall_clock epochTimer;
 };
 
 } // namespace ens
