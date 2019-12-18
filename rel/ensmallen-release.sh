9c545b7aaccb8e961aec79960a04f5d07442d8d0
Sayan Goswami <goswami.sayan47@gmail.com>
2019-12-18 20:09:20 +0530

    Auto update CONTRIBUTING.md test instructions on new release

--- rel/ensmallen-release.sh
+++ rel/ensmallen-release.sh
@@ -39,6 +39,9 @@ if [ "$#" -eq "4" ]; then
   sed -i 's/ENS_VERSION_NAME[ ]*\".*\"$/ENS_VERSION_NAME \"'"$4"'\"/' include/ensmallen_bits/ens_version.hpp;
 fi
 
+# update CONTRIBUTING.md
+sed -i "s/ensmallen-[0-9]*\.[0-9]*\.[0-9]*/ensmallen-$MAJOR.$MINOR.$PATCH/g" CONTRIBUTING.md;
+
 git pull
 git add include/ensmallen_bits/ens_version.hpp;
 git commit -m "Update and release version $MAJOR.$MINOR.$PATCH.";
