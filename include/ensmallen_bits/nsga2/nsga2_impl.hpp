99478eab8258f340f1fe8c671dc3507e87945336
Sayan Goswami <goswami.sayan47@gmail.com>
2019-12-22 17:47:23 +0530

    Add NSGA2 (WIP)

---
+++ include/ensmallen_bits/nsga2/nsga2_impl.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file nsga2_impl.hpp
+ * @author Sayan Goswami
+ *
+ * ensmallen is free software; you may redistribute it and/or modify it under
+ * the terms of the 3-clause BSD license.  You should have received a copy of
+ * the 3-clause BSD license along with ensmallen.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+
+#ifndef ENSMALLEN_NSGA2_NSGA2_HPP
+#define ENSMALLEN_NSGA2_NSGA2_HPP
+
+#include "nsga2.hpp"
+
+namespace ens {
+
+}
+
+#endif
