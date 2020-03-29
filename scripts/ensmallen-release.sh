70f6b1b36e50b51df2c19c835b6f5045161a6d82
Ryan Curtin <ryan@ratml.org>
2020-03-29 18:19:48 -0400

    Okay, one last test.

--- scripts/ensmallen-release.sh
+++ scripts/ensmallen-release.sh
@@ -7,17 +7,18 @@
 #
 # This should be run from the root of the repository.
 set -e
-set -x
 
 if [ "$#" -lt 4 ]; then
   echo "At least four arguments required!";
-  echo "$ ensmallen-release.sh <github username> <major> <minor> <patch> [<name>]";
+  echo "$ ensmallen-release.sh <github username> <major> <minor> <patch>" \
+      "[<name>]";
   exit 1;
 fi
 
 if [ "$#" -gt 5 ]; then
   echo "Too many arguments!";
-  echo "$ ensmallen-release.sh <github username> <major> <minor> <patch> [<name>]";
+  echo "$ ensmallen-release.sh <github username> <major> <minor> <patch>" \
+      "[<name>]";
   exit 1;
 fi
 
@@ -31,17 +32,24 @@ if [ "$lines" != "0" ]; then
 fi
 
 # Make sure that the mlpack repository exists.
-dest_remote_name=`git remote -v | grep "https://github.com/mlpack/ensmallen (fetch)" | head -1 | awk -F' ' '{ print $1 }'`;
+dest_remote_name=`git remote -v |\
+                  grep "https://github.com/mlpack/ensmallen (fetch)" |\
+                  head -1 |\
+                  awk -F' ' '{ print $1 }'`;
+
 if [ "a$dest_remote_name" == "a" ]; then
-    echo "No git remote found for https://github.com/mlpack/ensmallen!";
-    echo "Make sure that you've got the ensmallen repository as a remote, and that the master branch from that remote is checked out.";
-    echo "You can do this with a fresh repository via \`git clone https://github.com/mlpack/ensmallen\`.";
-    exit 1;
+  echo "No git remote found for https://github.com/mlpack/ensmallen!";
+  echo "Make sure that you've got the ensmallen repository as a remote, and" \
+      "that the master branch from that remote is checked out.";
+  echo "You can do this with a fresh repository via \`git clone" \
+      "https://github.com/mlpack/ensmallen\`.";
+  exit 1;
 fi
 
 # Also check that we're on the master branch, from the correct origin.
 current_branch=`git branch --no-color | grep '^\* ' | awk -F' ' '{ print $2 }'`;
-current_origin=`git rev-parse --abbrev-ref --symbolic-full-name @{u} | awk -F'/' '{ print $1 }'`;
+current_origin=`git rev-parse --abbrev-ref --symbolic-full-name @{u} |\
+                awk -F'/' '{ print $1 }'`;
 if [ "a$current_branch" != "amaster" ]; then
   echo "Current branch is $current_branch."
   echo "This script has to be run from the master branch."
@@ -57,22 +65,27 @@ fi
 hub_output="`which hub`" || true;
 echo "hub output is $hub_output";
 if [ "a$hub_output" == "a" ]; then
-  echo "The Hub command-line tool must be installed for this script to run successfully.";
-  echo "See https://hub.github.com for more details and installation instructions.";
+  echo "The Hub command-line tool must be installed for this script to run" \
+      "successfully.";
+  echo "See https://hub.github.com for more details and installation" \
+      "instructions.";
   echo "";
   echo "(apt-get install hub on Debian and Ubuntu)";
   echo "(brew install hub via Homebrew)";
   exit 1;
 fi
 
-# Check git remotes.
+# Check git remotes: we need to make sure we have a fork to push to.
 github_user=$1;
-remote_name=`git remote -v | grep "https://github.com/$github_user/ensmallen (push)" | head -1 | awk -F' ' '{ print $1 }'`;
+remote_name=`git remote -v |\
+             grep "https://github.com/$github_user/ensmallen (push)" |\
+             head -1 |\
+             awk -F' ' '{ print $1 }'`;
 if [ "a$remote_name" == "a" ]; then
-    echo "No git remote found for https://github.com/$github_user/ensmallen!";
-    echo "Adding remote '$github_user'.";
-    git remote add $github_user https://github.com/$github_user/ensmallen;
-    remote_name="$github_user";
+  echo "No git remote found for https://github.com/$github_user/ensmallen!";
+  echo "Adding remote '$github_user'.";
+  git remote add $github_user https://github.com/$github_user/ensmallen;
+  remote_name="$github_user";
 fi
 git fetch $github_user;
 
@@ -84,27 +97,35 @@ MAJOR=$2;
 MINOR=$3;
 PATCH=$4;
 
-sed -i 's/ENS_VERSION_MAJOR[ ]*[0-9]*$/ENS_VERSION_MAJOR '$MAJOR'/' include/ensmallen_bits/ens_version.hpp;
-sed -i 's/ENS_VERSION_MINOR[ ]*[0-9]*$/ENS_VERSION_MINOR '$MINOR'/' include/ensmallen_bits/ens_version.hpp;
-sed -i 's/ENS_VERSION_PATCH[ ]*[0-9]*$/ENS_VERSION_PATCH '$PATCH'/' include/ensmallen_bits/ens_version.hpp;
+sed -i 's/ENS_VERSION_MAJOR[ ]*[0-9]*$/ENS_VERSION_MAJOR '$MAJOR'/' \
+    include/ensmallen_bits/ens_version.hpp;
+sed -i 's/ENS_VERSION_MINOR[ ]*[0-9]*$/ENS_VERSION_MINOR '$MINOR'/' \
+    include/ensmallen_bits/ens_version.hpp;
+sed -i 's/ENS_VERSION_PATCH[ ]*[0-9]*$/ENS_VERSION_PATCH '$PATCH'/' \
+    include/ensmallen_bits/ens_version.hpp;
 
 if [ "$#" -eq "5" ]; then
-  sed -i 's/ENS_VERSION_NAME[ ]*\".*\"$/ENS_VERSION_NAME \"'"$5"'\"/' include/ensmallen_bits/ens_version.hpp;
+  sed -i 's/ENS_VERSION_NAME[ ]*\".*\"$/ENS_VERSION_NAME \"'"$5"'\"/' \
+      include/ensmallen_bits/ens_version.hpp;
 fi
 
 # Update CONTRIBUTING.md.
-sed -i "s/ensmallen-[0-9]*\.[0-9]*\.[0-9]*/ensmallen-$MAJOR.$MINOR.$PATCH/g" CONTRIBUTING.md;
+sed -i "s/ensmallen-[0-9]*\.[0-9]*\.[0-9]*/ensmallen-$MAJOR.$MINOR.$PATCH/g" \
+    CONTRIBUTING.md;
 
 # Update HISTORY.md with the release date and possibly name.
-version_name=`grep ENS_VERSION_NAME include/ensmallen_bits/ens_version.hpp | head -1 | sed 's/.*\"\(.*\)\"/\1/'`;
+version_name=`grep ENS_VERSION_NAME include/ensmallen_bits/ens_version.hpp |\
+              head -1 |\
+              sed 's/.*\"\(.*\)\"/\1/'`;
 year=`date +%Y`;
 month=`date +%m`;
 day=`date +%d`;
-sed -i "s/### ensmallen ?.??.?: \"???\"/### ensmallen $MAJOR.$MINOR.$PATCH: \"$version_name\"/" HISTORY.md;
+new_line="ensmallen $MAJOR.$MINOR.$PATCH: \"$version_name\"";
+sed -i "s/### ensmallen ?.??.?: \"???\"/### $new_line/" HISTORY.md;
 sed -i "s/###### ????-??-??/###### $year-$month-$day/" HISTORY.md;
 
 # Now, we'll do all this on a new release branch.
-git checkout -b release-$MAJOR-$MINOR-$PATCH;
+git checkout -b release-$MAJOR.$MINOR.$PATCH;
 
 git add include/ensmallen_bits/ens_version.hpp;
 git add CONTRIBUTING.md;
@@ -121,41 +142,21 @@ git add HISTORY.md;
 git commit -m "Add new block for next release to HISTORY.md.";
 
 # Push to new branch.
-git push --set-upstream $github_user release-$MAJOR-$MINOR-$PATCH;
+git push --set-upstream $github_user release-$MAJOR.$MINOR.$PATCH;
 
-# Next, we have to actually open the PR for the release.
+# Next, we have to actually open the PR for the release.  These lines would be
+# hard to wrap so they are longer than the length limit. :)
 hub pull-request \
     -b mlpack:master \
-    -h $github_user:release-$MAJOR-$MINOR-$PATCH \
-    -m "Release version $MAJOR-$MINOR-$PATCH" \
-    -m "This automatically-generated pull request adds the commits necessary to make the $MAJOR-$MINOR-$PATCH release." \
-    -m "" \
-    -m "Once the PR is merged, mlpack-bot will tag the release and publish it." \
+    -h $github_user:release-$MAJOR.$MINOR.$PATCH \
+    -m "Release version $MAJOR.$MINOR.$PATCH" \
+    -m "This automatically-generated pull request adds the commits necessary to make the $MAJOR.$MINOR.$PATCH release." \
+    -m "Once the PR is merged, mlpack-bot will tag the release as HEAD~1 (so that it doesn't include the new HISTORY block) and publish it." \
     -m "Or, well, hopefully that will happen someday." \
-    -m "" \
     -l "t: release"
 
-
+echo "";
+echo "Switching back to 'master' branch.";
+echo "If you want to access the release branch again, use \`git checkout" \
+    "release-$MAJOR.$MINOR.$PATCH\`.";
 exit 0;
-
-## everything below here isn't done...
-
-# Tag will be done by mlpack-bot.
-git tag $MAJOR.$MINOR.$PATCH;
-git push origin $MAJOR.$MINOR.$PATCH;
-git push origin master;
-
-git clone https://github.com/mlpack/ensmallen.org /tmp/ensmallen.org/;
-git archive --prefix=ensmallen-$MAJOR.$MINOR.$PATCH/ $MAJOR.$MINOR.$PATCH | gzip > /tmp/ensmallen.org/files/ensmallen-$MAJOR.$MINOR.$PATCH.tar.gz;
-cd /tmp/ensmallen.org/;
-git add files/ensmallen-$MAJOR.$MINOR.$PATCH.tar.gz;
-cd files/;
-rm ensmallen-latest.tar.gz;
-ln -s ensmallen-$MAJOR.$MINOR.$PATCH.tar.gz ensmallen-latest.tar.gz;
-cd ../
-git add files/ensmallen-latest.tar.gz;
-git commit -m "Release version $MAJOR.$MINOR.$PATCH.";
-git push origin;
-cd -
-
-rm -rf /tmp/ensmallen.org;
