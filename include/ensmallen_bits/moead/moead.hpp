7c4ba50afd5946dfbdb0b8be390bd56996668cf6
UtR491 <64355352+UtR491@users.noreply.github.com>
2020-07-29 16:29:25 +0530

    Added moead.hpp and moead_impl.hpp

---
+++ include/ensmallen_bits/moead/moead.hpp
@@ -0,0 +1,228 @@
+/**
+ * @file moead.hpp
+ * @author Utkarsh Rai
+ *
+ * MOEA/D, Multi Objective Evolutionary Algorithm based on Decompositon is a
+ * multi objective optimization algorithm. It employs evolutionary algorithms,
+ * to find better solutions by iterating on the previous solutions and
+ * decomposition approaches, to convert the multi objective problem to a single
+ * objective one, to find the best Pareto Front for the given problem.
+ *
+ * ensmallen is free software; you may redistribute it and/or modify it under
+ * the terms of the 3-clause BSD license.  You should have received a copy of
+ * the 3-clause BSD license along with ensmallen.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more information.
+ */
+
+#ifndef ENSMALLEN_MOEAD_MOEAD_HPP
+#define ENSMALLEN_MOEAD_MOEAD_HPP
+
+namespace ens {
+
+/**
+ * This class implements the MOEA/D algorithm. Step numbers used in different
+ * parts of the implementation correspond to the step number used in the
+ * original algorithm by the author.
+ *
+ * For more information, see the following:
+ * @code
+ * @articl{article,
+ * author = {Zhang, Qingfu and Li, Hui},
+ * year = {2008},
+ * month = {01},
+ * pages = {712 - 731},
+ * title = {MOEA/D: A Multiobjective Evolutionary Algorithm Based on
+ *    Decomposition},
+ * volume = {11},
+ * journal = {Evolutionary Computation, IEEE Transactions on},
+ * doi = {10.1109/TEVC.2007.892759}}
+ * @endcode
+ *
+ * MOEA/D can optimize arbitrary multi-objective functions. For more details,
+ * see the documentation on function types included with this distribution or
+ * on the ensmallen website.
+ */
+
+class MOEAD {
+  public:
+    /**
+     * Constructor for the MOEA/D optimizer.
+     *
+     * The default values provided here are not necessarily suitable for a
+     * given function. Therefore, it is highly recommended to adjust the
+     * parameters according to the problem.
+     *
+     * @param populationSize, the number of elements in the population.
+     * @param crossoverProb The probability that a crossover will occur.
+     * @param mutationProb The probability that a mutation will occur.
+     * @param T, the number of nearest neighbours of weights to find.
+     * @param lowerBound, the lower bound on each variable of a member
+     *    of the variable space.
+     * @param upperBound, the upper bound on each variable of a member
+     *    of the variable space.
+     */
+    MOEAD(const size_t populationSize = 100,
+          const double crossoverProb = 0.6,
+          const double mutationProb = 0.3,
+          const double mutationStrength = 1e-3,
+          const size_t T = 50,
+          const arma::vec& lowerBound = arma::ones(1, 1),
+          const arma::vec& upperBound = arma::ones(1, 1));
+    /**
+     * Optimize a set of objectives. The initial population is generated
+     * using the initial point. The output is the best generated front.
+     *
+     * @tparam MatType, the type of matrix used to store coordinates.
+     * @tparam ArbitraryFunctionType, the type of objective function.
+     * @param objectives, std::tuple of the objective functions.
+     * @param iterate, the initial reference point for generating population.
+     */
+    template<typename MatType,
+             typename... ArbitraryFunctionType>
+    typename MatType::elem_type Optimize(std::tuple<ArbitraryFunctionType...>& objectives,
+                                MatType& iterate);
+
+    //! Get the population size.
+    size_t PopulationSize() const { return populationSize; }
+    //! Modify the population size.
+    size_t& PopulationSize() { return populationSize; }
+
+    //! Get the crossover rate.
+    double CrossoverRate() const { return crossoverProb; }
+    //! Modify the crossover rate.
+    double& CrossoverRate() { return crossoverProb; }
+
+    //! Get the mutation probability.
+    double MutationProbability() const { return mutationProb; }
+    //! Modify the mutation probability.
+    double& MutationProbability() { return mutationProb; }
+
+    //! Retrieve value of lowerBound.
+    const arma::vec& LowerBound() const { return lowerBound; }
+    //! Modify value of lowerBound.
+    arma::vec& LowerBound() { return lowerBound; }
+
+    //! Retrieve value of upperBound.
+    const arma::vec& UpperBound() const { return upperBound; }
+    //! Modify value of upperBound.
+    arma::vec& UpperBound() { return upperBound; }
+
+    //! Retrieve the best front (the Pareto frontier).  This returns an empty
+    //! vector until `Optimize()` has been called.
+    const std::vector<arma::mat>& Front() const { return bestFront; }
+
+  private:
+    /**
+     * Mutate child formed by the crossover of two random members of the 
+     * population.
+     *
+     * @tparam MatType, the type of matrix used to store coordinates.
+     * @param child, the matrix to mutate.
+     * @param lowerBound, the lower bound on each variable in the matrix.
+     * @param upperBound, the upper bound on each variable in the matrix.
+     */
+    template<typename MatType>
+      void Mutate(MatType& child,
+                const arma::vec& lowerBound,
+                const arma::vec& upperBound);
+
+    /**
+     * Decompose the multi objective problem to a single objetive problem
+     * using the Tchebycheff approach.
+     *
+     * @param weights, a set of real number which act as weights.
+     * @param idealPoint, ideal point z in Tchebycheff decomposition.
+     * @param evaluatedCandidate, value of the candidate per objective.
+     * @return the single value obtained from decomposed function.
+     */
+    double DecomposedSingleObjective(const arma::vec& weights,
+                                     const arma::vec& idealPoint,
+                                     const arma::vec& evaluatedCandidate);
+
+    /**
+     * Check domination between two vectors.
+     *
+     * @param first, the first of the two vectors to compare.
+     * @param second, the second of the two vectors to compare.
+     * @return true if first dominates second else false.
+     */
+    bool Dominates(const arma::vec& first,
+        const arma::vec& second);
+
+    /**
+     * Evaluate objectives for the elite population.
+     *
+     * @tparam ArbitraryFunctionType std::tuple of multiple function types.
+     * @tparam MatType Type of matrix to optimize.
+     * @param population The elite population.
+     * @param objectives The set of objectives.
+     * @param calculatedObjectives Vector to store calculated objectives.
+     */    
+    template<std::size_t I = 0,
+    typename MatType,
+             typename ...ArbitraryFunctionType>
+               typename std::enable_if<I == sizeof...(ArbitraryFunctionType),
+               void>::type EvaluateObjectives(std::vector<MatType>&,
+                   std::tuple<ArbitraryFunctionType...>&,
+                   arma::mat&);
+
+    template<std::size_t I = 0,
+      typename MatType,
+      typename ...ArbitraryFunctionType>
+        typename std::enable_if<I < sizeof...(ArbitraryFunctionType), void>::type
+        EvaluateObjectives(std::vector<MatType>& population,
+            std::tuple<ArbitraryFunctionType...>& objectives,
+            arma::mat& calculatedObjectives);
+    template<std::size_t I = 0,
+      typename MatType,
+      typename ...ArbitraryFunctionType>
+        typename std::enable_if<I == sizeof...(ArbitraryFunctionType), void>::type
+        EvaluateObjectives(std::vector<MatType>&,
+            std::tuple<ArbitraryFunctionType...>&,
+            std::vector<arma::vec>&);
+
+    template<std::size_t I = 0,
+      typename MatType,
+      typename ...ArbitraryFunctionType>
+        typename std::enable_if<I < sizeof...(ArbitraryFunctionType), void>::type
+        EvaluateObjectives(std::vector<MatType>& population,
+            std::tuple<ArbitraryFunctionType...>& objectives,
+            std::vector<arma::vec>& calculatedObjectives);
+
+    //! Size of the population.
+    size_t populationSize;
+
+    //! Probability of crossover between two members.
+    double crossoverProb;
+
+    //! Probability of mutation of a child.
+    double mutationProb;
+
+    //! Strength of mutation.
+    double mutationStrength;
+
+    //! Number of nearest neighbours of weights to consider.
+    size_t T;
+
+    //! Lower bound on each variable in the variable space.
+    arma::vec lowerBound;
+
+    //! Upper bound on each variable in the variable space.
+    arma::vec upperBound;
+
+    //delete.
+    double DEParameter;
+
+    //! The number of objectives in multi objective optimisation problem.
+    size_t numObjectives;
+
+    //! The Pareto Optimal Front.
+    std::vector<arma::mat> bestFront;
+};
+
+} // namespace ens
+
+// Include implementation.
+#include "moead_impl.hpp"
+
+#endif
--- include/ensmallen_bits/moead/moead_impl.hpp
+++ include/ensmallen_bits/moead/moead_impl.hpp
@@ -1,59 +1,343 @@
+/**
+ * @file moead_impl.hpp
+ * @author Utkarsh Rai
+ *
+ * Implementation of the MOEA/D algorithm. Used for multi-objective
+ * optimization problems on arbitrary functions.
+ *
+ * ensmallen is free software; you may redistribute it and/or modify it under
+ * the terms of the 3-clause BSD license.  You should have received a copy of
+ * the 3-clause BSD license along with ensmallen.  If not, see
+ * http://www.opensource.org/licenses/BSD-3-Clause for more Information.
+ */
+
+#ifndef ENSMALLEN_MOEAD_MOEAD_IMPL_HPP
+#define ENSMALLEN_MOEAD_MOEAD_IMPL_HPP
+
 #include "moead.hpp"
 
 namespace ens {
 
 inline MOEAD::MOEAD(const size_t populationSize,
-                    const size_t maxGenerations,
                     const double crossoverProb,
                     const double mutationProb,
                     const double mutationStrength,
-                    const double epsilon,
-                    const size_t numWeights,
-                    const size_t T) :
+                    const size_t T,
+                    const arma::vec& lowerBound,
+                    const arma::vec& upperBound) :
     populationSize(populationSize),
-    maxGenerations(maxGenerations),
     crossoverProb(crossoverProb),
     mutationProb(mutationProb),
     mutationStrength(mutationStrength),
-    epsilon(epsilon),
-    T(T)
+    T(T),
+    lowerBound(lowerBound),
+    upperBound(upperBound)
   { /* Nothing to do here. */ }
 
+//! Optimize the function.
 template<typename MatType,
          typename... ArbitraryFunctionType>
-arma::vec<MatType> MOEAD::Optimize(std::tuple<ArbitraryFunctionType...>& objectives,
+typename MatType::elem_type MOEAD::Optimize(std::tuple<ArbitraryFunctionType...>& objectives,
                                    MatType& iterate)
 {
+  // Convenience typedefs.
+  typedef typename MatType::elem_type ElemType;
+
+  if(lowerBound.size() != upperBound.size())
+  {
+    throw std::logic_error("MOEAD::Optimize(): size of lowerBound and upperBound "
+        "must be equal.");
+  }
+  if(lowerBound.size() != iterate.n_elem)
+  {
+    throw std::logic_error("MOEAD::Optimize(): there should be a lower bound and "
+        "an upper bound for each variable in the initial point.");
+  }
   // Number of objective functions. Represented by m in the paper.
   numObjectives = sizeof...(ArbitraryFunctionType);
-  // The external population, non-dominated solutions.
-  arma::vec<MatType> externalPopulation;
+
+  // 1.1 The external population, non-dominated solutions.
+  std::vector<MatType> externalPopulation;
+
   // Weight vectors, where each one of them represents a decomposition.
-  arma::mat weights(numWeights, numObjectives, fill::randu);
-  // Storing the T nearest neighbours of each weight vector.
-  arma::mat<size_t> B(numWeights, T);
-  for(int i = 0; i < numWeights; i++)
-  {
-    arma::vec distances(numWeights);
-    for(int j = 0; j < numWeights; j++)
-    {
-      double distance = 0;
-      for(int w = 0; w < numObjectives ; w++)
-      {
-        distance += std::pow(weights[i][w] - weights[j][w], 2);
-      }
-      distance = std::sqrt(distance);
-      distances[w]=distance;
-    }
-    for(int iter = 1; iter<=T; iter++)
-      B(i, iter) = arma::stable_sort_index(distances)(iter);
-  }
-  // Random generation of the initial population.
-  arma::vec<MatType> population(populationSize);
+  std::vector<arma::vec> weights(populationSize);
+  for(size_t i =0; i<populationSize; i++)
+    weights[i] = arma::vec(numObjectives, arma::fill::randu);
+
+  // 1.2 Storing the indices of T nearest neighbours of each weight vector.
+  arma::Mat<size_t> B(populationSize, T);
   for(size_t i = 0; i < populationSize; i++)
   {
-    population(i) = arma::randu<MatType>(iterate.n_rows, iterate.n_cols) - 0.5 + iterate;
+    // To temporarily store the distance between weights(i) and each other weights.
+    arma::vec distances(populationSize);
+    for(size_t j = 0; j < populationSize; j++)
+    {
+      distances(j) = 0;
+      for(size_t w = 0; w < numObjectives ; w++)
+      {
+        distances(j) += std::pow(weights[i](w) - weights[j](w), 2);
+      }
+      distances(j)=std::sqrt(distances(j));
+    }
+    arma::uvec sortedIndices = arma::stable_sort_index(distances, "descend");
+    for(size_t iter = 1; iter<=T; iter++)
+      B(i, iter-1) = sortedIndices(iter);
   }
-  arma::mat FValue(populationSize, numObjectives);
 
+  // 1.3 Random generation of the initial population.
+  std::vector<MatType> population(populationSize);
+  for(size_t i = 0; i < populationSize; i++)
+    population[i] = arma::randu<MatType>(iterate.n_rows, iterate.n_cols)
+        - 0.5 + iterate;
+
+  // 1.3 F-value initialisation for the population.
+  std::vector<arma::vec> FValue(populationSize);
+  for ( size_t i = 0; i<populationSize; i++)
+    FValue[i].resize(numObjectives);
+  EvaluateObjectives(population, objectives, FValue);
+
+  // 1.4 Initialize the ideal point z.
+  arma::vec idealPoint(numObjectives);
+  idealPoint.fill(-1/.0);
+
+  // 2 The main loop.
+  for(size_t i =0; i<populationSize; i++)
+  {
+    // To generate random numbers.
+    std::default_random_engine generator;
+
+    // Distribution to choose two random parents for mutation.
+    std::uniform_int_distribution<size_t> distribution(0, populationSize-1);
+
+    // Distribution to choose a number between 0 and 1 to determine whether
+    // mutation or crossover will happen or not.
+    std::uniform_real_distribution<double> crossoverDeterminer(0, 1);
+
+    // 2.1 Randomly select two indices in B(i) and use them to make a child.
+    size_t k = distribution(generator), l =distribution(generator);
+    if (k == l)
+    {
+      if (k==populationSize-1)
+        k-=1;
+      else
+        k+=1;
+    }
+    MatType candidate;
+    if(crossoverDeterminer(generator)<crossoverProb)
+    {
+      candidate.resize(iterate.n_rows, iterate.n_cols);
+      for(size_t idx =0;idx<iterate.n_rows; idx++)
+      {
+        if(crossoverDeterminer(generator)<0.5)
+          candidate[idx] = population[k][idx];
+        else
+          candidate[idx] = population[l][idx];
+        if(candidate[idx]<lowerBound(idx))
+          candidate[idx] = lowerBound(idx);
+        if(candidate[idx]>upperBound(idx))
+          candidate[idx] = upperBound(idx);
+      }
+    }
+    else
+      candidate = population[i];
+
+    // 2.2 Improve the child.
+    Mutate(candidate, lowerBound, upperBound);
+
+    // Store solution for candidate.
+    arma::mat evaluatedCandidate(1, numObjectives);
+    std::vector<MatType> candidateVector(1);
+    candidateVector[0]=candidate;
+    EvaluateObjectives(candidateVector, objectives, evaluatedCandidate);
+    arma::vec evalCandidate(numObjectives);
+
+    // 2.3 Update of ideal point.
+    for(size_t idx = 0;idx<numObjectives;idx++)
+    {
+      idealPoint(idx)=std::max(idealPoint(idx),
+          evaluatedCandidate(0, idx));
+      evalCandidate(idx) = evaluatedCandidate(0, idx);
+    }
+
+    // 2.4 Update of the neighbouring solutions.
+    for(size_t idx=0;idx<T;idx++)
+    {
+      if(DecomposedSingleObjective(weights[B(i, idx)],
+                                   idealPoint, evalCandidate)
+            <=DecomposedSingleObjective(weights[B(i,idx)],
+                                        idealPoint, FValue[B(i, idx)]))
+      {
+        population.at(B(i, idx)) = candidate;
+        FValue[B(i, idx)] = evalCandidate;
+      }
+    }
+
+    // 2.5 Updating External Population.
+    if ( !externalPopulation.empty() )
+    {
+      std::vector<arma::vec> first(1);
+      auto df = [&](MatType firstMat) -> bool
+      {
+        std::vector<MatType> wrapperFirst(1), wrapperSecond(1);
+        wrapperFirst[0]=firstMat;
+        first[0].resize(numObjectives);
+        EvaluateObjectives(wrapperFirst, objectives, first);
+        return Dominates(evalCandidate, first[0]);
+      };
+      //! Remove the part that is dominated by candidate.
+      externalPopulation.erase(
+          std::remove_if(
+          externalPopulation.begin(),
+          externalPopulation.end(),
+          df), externalPopulation.end());
+
+      //! Check if any of the remaining members of external population dominate
+      //! candidate.
+      bool flag = 0;
+      for( size_t idx = 0; idx < externalPopulation.size(); idx++)
+      {
+        if (Dominates(first[0], evalCandidate))
+        {
+          flag = 1;
+          break;
+        }
+      }
+      if(flag==0) externalPopulation.push_back(candidate);
+    }
+    else
+    {
+      externalPopulation.push_back(candidate);
+    }
+  }
+  bestFront = std::move(externalPopulation);
+
+  ElemType performance = std::numeric_limits<ElemType>::max();
+
+  for(arma::Col<ElemType> objective: FValue)
+    if (arma::accu(objective) < performance)
+      performance = arma::accu(objective);
+
+  return performance;
 }
+
+//! Perform mutation of the candidate.
+  template<typename MatType>
+inline void MOEAD::Mutate(MatType& child,
+    const arma::vec& lowerBound,
+    const arma::vec& upperBound)
+{
+  child += (arma::randu<MatType>(child.n_rows, child.n_cols) < mutationProb) %
+    (mutationStrength * arma::randn<MatType>(child.n_rows, child.n_cols));
+
+  // Constraint all genes to be between bounds.
+  for (size_t idx = 0; idx < child.n_rows; idx++)
+  {
+    if (child[idx] < lowerBound(idx))
+      child[idx] = lowerBound(idx);
+    else if (child[idx] > upperBound(idx))
+      child[idx] = upperBound(idx);
+  }
+}
+
+//! Calculate the output for single objective function using the Tchebycheff
+//! approach.
+inline double MOEAD::DecomposedSingleObjective(const arma::vec& weights,
+    const arma::vec& idealPoint,
+    const arma::vec& evaluatedCandidate)
+{
+  double value = weights(0)*std::abs(evaluatedCandidate(0)
+      - idealPoint(0));
+  for (size_t i = 1; i < numObjectives; i++)
+  {
+    value = std::min(value, weights(i)*std::abs(
+          evaluatedCandidate(i) - idealPoint(i)));
+  }
+  return value;
+}
+
+//! To check if first dominates second.
+inline bool MOEAD::Dominates(const arma::vec& first,
+    const arma::vec& second)
+{
+  size_t i; int flag = 0;
+  for ( i = 0; i < numObjectives; i++)
+  {
+    if ( first(i) < second(i) )
+    {
+      return false;
+    }
+    if( first(i) > second(i) )
+      flag = 1;
+  }
+  if ( i==numObjectives && flag ==1 )
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+//! No objectives to evaluate.
+template<std::size_t I,
+  typename MatType,
+  typename ...ArbitraryFunctionType>
+  typename std::enable_if<I == sizeof...(ArbitraryFunctionType), void>::type
+MOEAD::EvaluateObjectives(
+    std::vector<MatType>&,
+    std::tuple<ArbitraryFunctionType...>&,
+    arma::mat &)
+{
+  // Nothing to do here.
+}
+
+//! Evaluate the objectives for the entire population.
+template<std::size_t I,
+  typename MatType,
+  typename ...ArbitraryFunctionType>
+  typename std::enable_if<I < sizeof...(ArbitraryFunctionType), void>::type
+MOEAD::EvaluateObjectives(
+    std::vector<MatType>& population,
+    std::tuple<ArbitraryFunctionType...>& objectives,
+    arma::mat& calculatedObjectives)
+{
+  for (size_t i = 0; i < population.size(); i++)  {
+    calculatedObjectives(i, I) = std::get<I>(objectives).Evaluate(population[i]);
+    EvaluateObjectives<I+1, MatType, ArbitraryFunctionType...>(population, objectives,
+        calculatedObjectives);
+  }
+}
+
+//! No objectives to evaluate.
+template<std::size_t I,
+  typename MatType,
+  typename ...ArbitraryFunctionType>
+  typename std::enable_if<I == sizeof...(ArbitraryFunctionType), void>::type
+MOEAD::EvaluateObjectives(
+    std::vector<MatType>&,
+    std::tuple<ArbitraryFunctionType...>&,
+    std::vector<arma::vec>&)
+{
+  // Nothing to do here.
+}
+
+//! Evaluate the objectives for the entire population.
+template<std::size_t I,
+  typename MatType,
+  typename ...ArbitraryFunctionType>
+  typename std::enable_if<I < sizeof...(ArbitraryFunctionType), void>::type
+MOEAD::EvaluateObjectives(
+    std::vector<MatType>& population,
+    std::tuple<ArbitraryFunctionType...>& objectives,
+    std::vector<arma::vec>& calculatedObjectives)
+{
+  for (size_t i = 0; i < population.size(); i++)  {
+    calculatedObjectives[i](I) = std::get<I>(objectives).Evaluate(population[i]);
+    EvaluateObjectives<I+1, MatType, ArbitraryFunctionType...>(population, objectives,
+        calculatedObjectives);
+  }
+}
+}
+
+#endif
