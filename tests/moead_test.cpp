1f353336d78ea9237e222a835fc6f5b21c0e2831
UtR491 <64355352+UtR491@users.noreply.github.com>
2020-07-29 17:03:14 +0530

    Changed the filename in comments form nsga2_test to moead_test

--- tests/moead_test.cpp
+++ tests/moead_test.cpp
@@ -1,7 +1,8 @@
 
 /**
- * @file nsga2_test.cpp
+ * @file moead_test.cpp
  * @author Sayan Goswami
+ * @author Utkarsh Rai
  *
  * ensmallen is free software; you may redistribute it and/or modify it under
  * the terms of the 3-clause BSD license.  You should have received a copy of
