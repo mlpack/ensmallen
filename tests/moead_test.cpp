ee470ba5262b30ca54bd7a9f89f8d019f1a859f0
UtR491 <64355352+UtR491@users.noreply.github.com>
2020-09-03 23:57:48 +0530

    Update moead_test.cpp

--- tests/moead_test.cpp
+++ tests/moead_test.cpp
@@ -38,6 +38,43 @@ bool InBounds(const double& value, const double& low, const double& high)
 TEST_CASE("MOEADFonsecaFlemingTest", "[MOEADTest]")
 {
   FonsecaFlemingFunction<arma::mat> FON;
+  const double lowerBound = -4;
+  const double upperBound = 4;
+  const double strength = 1e-3;
+  const double expectedLowerBound = -1.0 / sqrt(3);
+  const double expectedUpperBound = 1.0 / sqrt(3);
+  MOEAD opt(150, 10, 0.6, 0.7, strength, 10, 0.5, lowerBound, upperBound);
+
+  typedef decltype(FON.objectiveA) ObjectiveTypeA;
+  typedef decltype(FON.objectiveB) ObjectiveTypeB;
+  arma::mat coords = FON.GetInitialPoint();
+  std::tuple<ObjectiveTypeA, ObjectiveTypeB> objectives = FON.GetObjectives();
+  opt.Optimize(objectives, coords);
+  std::vector<arma::mat> bestFront = opt.Front();
+  bool allInRange = true;
+  for (size_t i = 0; i < bestFront.size(); i++)
+  {
+    const arma::mat solution = bestFront[i];
+    double valX = arma::as_scalar(solution(0));
+    double valY = arma::as_scalar(solution(1));
+    double valZ = arma::as_scalar(solution(2));
+
+    if (!InBounds(valX, expectedLowerBound, expectedUpperBound) ||
+        !InBounds(valY, expectedLowerBound, expectedUpperBound) ||
+        !InBounds(valZ, expectedLowerBound, expectedUpperBound))
+    {
+      allInRange = false;
+      break;
+    }
+  }
+  REQUIRE(allInRange);
+}
+/**
+ * Optimize for the Fonseca Fleming function using MOEA/D optimizer.
+ */
+TEST_CASE("MOEADFonsecaFlemingVectorBoundsTest", "[MOEADTest]")
+{
+  FonsecaFlemingFunction<arma::mat> FON;
   const arma::vec lowerBound("-4 -4 -4");
   const arma::vec upperBound("4 4 4");
   const double strength = 1e-3;
@@ -69,17 +106,16 @@ TEST_CASE("MOEADFonsecaFlemingTest", "[MOEADTest]")
   }
   REQUIRE(allInRange);
 }
-
 /**
  * Optimize for the function using MOEA/D optimizer.
  */
 TEST_CASE("MOEADSchafferN1Test", "[MOEADTest]")
 {
     SchafferFunctionN1<arma::mat> SCH;
-    arma::vec lowerBound = {-1000};
-    arma::vec upperBound = {1000};
+    double lowerBound = {-1000};
+    const double upperBound = {1000};
 
-    MOEAD opt(150, 10, 0.6, 0.7, 1e-3, 9, 0.8, lowerBound, upperBound);
+    MOEAD opt(150, 10, 0.6, 0.7, 1e-3, 9, 0.5, lowerBound, upperBound);
 
     typedef decltype(SCH.objectiveA) ObjectiveTypeA;
     typedef decltype(SCH.objectiveB) ObjectiveTypeB;
@@ -91,22 +127,54 @@ TEST_CASE("MOEADSchafferN1Test", "[MOEADTest]")
     opt.Optimize(objectives, coords);
     std::vector<arma::mat> bestFronts = opt.Front();
 
-    double minimumPositive = 1000;
     bool allInRange = true;
+    double minimumPositive = 1000;
 
     for (arma::mat solution: bestFronts)
     {
-      double val = arma::as_scalar(solution);
-      minimumPositive=min(minimumPositive, val>=0?val:1000);
-      if (val < 0.0 || val > 2.0)
+      const double val = arma::as_scalar(solution);
+      minimumPositive = std::min(minimumPositive, val>=0?val:1000);
+      if ((val < 0.00 && std::abs(val) >= minimumPositive) || val > 2.0)
       {
-        if(val>2.0 || (val<0.0 && std::abs(val)>minimumPositive))
-        {
-          allInRange = false;
-          break;
-        }
+        allInRange = false;
+        break;
       }
     }
   REQUIRE(allInRange);
 }
+/**
+ * Optimize for the function using MOEA/D optimizer.
+ */
+TEST_CASE("MOEADSchafferN1VectorBoundsTest", "[MOEADTest]")
+{
+    SchafferFunctionN1<arma::mat> SCH;
+    arma::vec lowerBound = {-1000};
+    arma::vec upperBound = {1000};
 
+    MOEAD opt(150, 10, 0.6, 0.7, 1e-3, 9, 0.5, lowerBound, upperBound);
+
+    typedef decltype(SCH.objectiveA) ObjectiveTypeA;
+    typedef decltype(SCH.objectiveB) ObjectiveTypeB;
+
+    arma::mat coords = SCH.GetInitialPoint();
+    std::tuple<ObjectiveTypeA, ObjectiveTypeB> objectives =
+        SCH.GetObjectives();
+
+    opt.Optimize(objectives, coords);
+    std::vector<arma::mat> bestFronts = opt.Front();
+
+    bool allInRange = true;
+    double minimumPositive = 1000;
+
+    for (arma::mat solution: bestFronts)
+    {
+      double val = arma::as_scalar(solution);
+      minimumPositive = std::min(minimumPositive, val>=0?val:1000);
+      if ((val < 0.00 && std::abs(val) >= minimumPositive) || val > 2.0)
+      {
+        allInRange = false;
+        break;
+      }
+    }
+  REQUIRE(allInRange);
+}
