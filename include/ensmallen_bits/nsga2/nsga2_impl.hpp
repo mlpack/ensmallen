5afdcdd9e35acb2c643d4a14ab01e2f4d942e422
NanuSai <gnanasai.n18@iiits.in>
2021-05-29 11:50:31 +0530

    replace callback in nsga2

--- include/ensmallen_bits/nsga2/nsga2_impl.hpp
+++ include/ensmallen_bits/nsga2/nsga2_impl.hpp
@@ -155,7 +155,8 @@ typename MatType::elem_type NSGA2::Optimize(
   for (size_t generation = 1; generation <= maxGenerations && !terminate; generation++)
   {
     Info << "NSGA2: iteration " << generation << "." << std::endl;
-    terminate |= Callback::StepTaken(*this, objectives, iterate, callbacks...);
+    terminate |= Callback::StepTaken(*this, objectives, iterate,
+        calculatedObjectives, fronts, callbacks...);
 
     // Create new population of candidate from the present elite population.
     // Have P_t, generate G_t using P_t.
