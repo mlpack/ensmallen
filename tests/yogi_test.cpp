3383542a4a05a07ec294599f0641d193f220b809
Marcus Edel <marcus.edel@fu-berlin.de>
2022-02-15 22:12:42 -0500

    Update test suits to use updated test framework.

--- tests/yogi_test.cpp
+++ tests/yogi_test.cpp
@@ -50,31 +50,8 @@ TEST_CASE("YogiSphereFunctionTestFMat", "[YogiTest]")
  */
 TEST_CASE("YogiMcCormickFunctionTest", "[YogiTest]")
 {
-  McCormickFunction f;
   Yogi optimizer(0.5, 1, 0.7, 0.999, 1e-8, 500000, 1e-5, false);
-
-  arma::mat coordinates = f.GetInitialPoint();
-  optimizer.Optimize(f, coordinates);
-
-  // 3% error tolerance.
-  REQUIRE(coordinates(0) == Approx(-0.547).epsilon(0.03));
-  REQUIRE(coordinates(1) == Approx(-1.547).epsilon(0.03));
-}
-
-/**
- * Tests the Yogi optimizer using a simple test function.
- */
-TEST_CASE("SimpleYogiTestFunction", "[YogiTest]")
-{
-  SGDTestFunction f;
-  Yogi optimizer(1e-3, 1, 0.9, 0.999, 1e-8, 500000, 1e-9, true);
-
-  arma::mat coordinates = f.GetInitialPoint();
-  optimizer.Optimize(f, coordinates);
-
-  REQUIRE(coordinates(0) == Approx(0.0).margin(0.3));
-  REQUIRE(coordinates(1) == Approx(0.0).margin(0.3));
-  REQUIRE(coordinates(2) == Approx(0.0).margin(0.3));
+  FunctionTest<McCormickFunction>(optimizer, 0.5, 0.1);
 }
 
 /**
@@ -82,24 +59,8 @@ TEST_CASE("SimpleYogiTestFunction", "[YogiTest]")
  */
 TEST_CASE("YogiLogisticRegressionTest", "[YogiTest]")
 {
-  arma::mat data, testData, shuffledData;
-  arma::Row<size_t> responses, testResponses, shuffledResponses;
-
-  LogisticRegressionTestData(data, testData, shuffledData,
-      responses, testResponses, shuffledResponses);
-  LogisticRegression<> lr(shuffledData, shuffledResponses, 0.5);
-
-  Yogi yogi;
-  arma::mat coordinates = lr.GetInitialPoint();
-  yogi.Optimize(lr, coordinates);
-
-  // Ensure that the error is close to zero.
-  const double acc = lr.ComputeAccuracy(data, responses, coordinates);
-  REQUIRE(acc == Approx(100.0).epsilon(0.003)); // 0.3% error tolerance.
-
-  const double testAcc = lr.ComputeAccuracy(testData, testResponses,
-      coordinates);
-  REQUIRE(testAcc == Approx(100.0).epsilon(0.006)); // 0.6% error tolerance.
+  Yogi optimizer;
+  LogisticRegressionFunctionTest(optimizer, 0.003, 0.006);
 }
 
 /**
@@ -108,22 +69,6 @@ TEST_CASE("YogiLogisticRegressionTest", "[YogiTest]")
  */
 TEST_CASE("YogiLogisticRegressionFMatTest", "[YogiTest]")
 {
-  arma::fmat data, testData, shuffledData;
-  arma::Row<size_t> responses, testResponses, shuffledResponses;
-
-  LogisticRegressionTestData(data, testData, shuffledData,
-      responses, testResponses, shuffledResponses);
-  LogisticRegression<arma::fmat> lr(shuffledData, shuffledResponses, 0.5);
-
   Yogi optimizer;
-  arma::fmat coordinates = lr.GetInitialPoint();
-  optimizer.Optimize(lr, coordinates);
-
-  // Ensure that the error is close to zero.
-  const float acc = lr.ComputeAccuracy(data, responses, coordinates);
-  REQUIRE(acc == Approx(100.0).epsilon(0.03)); // 3% error tolerance.
-
-  const float testAcc = lr.ComputeAccuracy(testData, testResponses,
-      coordinates);
-  REQUIRE(testAcc == Approx(100.0).epsilon(0.06)); // 6% error tolerance.
+  LogisticRegressionFunctionTest<arma::fmat>(optimizer, 0.003, 0.006);
 }
