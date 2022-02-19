ae8bd96620596030cae0e36b699cbf94ab948354
Marcus Edel <marcus.edel@fu-berlin.de>
2022-02-18 21:52:19 -0500

    Update test suits to use updated test framework.

--- tests/ada_sqrt_test.cpp
+++ tests/ada_sqrt_test.cpp
@@ -36,24 +36,8 @@ TEST_CASE("SimpleAdaGradTestFunction", "[AdaGradTest]")
  */
 TEST_CASE("AdaGradLogisticRegressionTest", "[AdaGradTest]")
 {
-  arma::mat data, testData, shuffledData;
-  arma::Row<size_t> responses, testResponses, shuffledResponses;
-
-  LogisticRegressionTestData(data, testData, shuffledData,
-      responses, testResponses, shuffledResponses);
-  LogisticRegression<> lr(shuffledData, shuffledResponses, 0.5);
-
   AdaGrad adagrad(0.99, 32, 1e-8, 5000000, 1e-9, true);
-  arma::mat coordinates = lr.GetInitialPoint();
-  adagrad.Optimize(lr, coordinates);
-
-  // Ensure that the error is close to zero.
-  const double acc = lr.ComputeAccuracy(data, responses, coordinates);
-  REQUIRE(acc == Approx(100.0).epsilon(0.003)); // 0.3% error tolerance.
-
-  const double testAcc = lr.ComputeAccuracy(testData, testResponses,
-      coordinates);
-  REQUIRE(testAcc == Approx(100.0).epsilon(0.006)); // 0.6% error tolerance.
+  LogisticRegressionFunctionTest(adagrad, 0.003, 0.006);
 }
 
 /**
@@ -86,22 +70,6 @@ TEST_CASE("SimpleAdaGradTestFunctionFMat", "[AdaGradTest]")
  */
 TEST_CASE("AdaGradLogisticRegressionTestFMat", "[AdaGradTest]")
 {
-  arma::fmat data, testData, shuffledData;
-  arma::Row<size_t> responses, testResponses, shuffledResponses;
-
-  LogisticRegressionTestData(data, testData, shuffledData,
-      responses, testResponses, shuffledResponses);
-  LogisticRegression<arma::fmat> lr(shuffledData, shuffledResponses, 0.5);
-
   AdaGrad adagrad(0.99, 32, 1e-8, 5000000, 1e-9, true);
-  arma::fmat coordinates = lr.GetInitialPoint();
-  adagrad.Optimize(lr, coordinates);
-
-  // Ensure that the error is close to zero.
-  const double acc = lr.ComputeAccuracy(data, responses, coordinates);
-  REQUIRE(acc == Approx(100.0).epsilon(0.003)); // 0.3% error tolerance.
-
-  const double testAcc = lr.ComputeAccuracy(testData, testResponses,
-      coordinates);
-  REQUIRE(testAcc == Approx(100.0).epsilon(0.006)); // 0.6% error tolerance.
+  LogisticRegressionFunctionTest<arma::fmat>(adagrad, 0.003, 0.006);
 }
