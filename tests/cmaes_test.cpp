f7574dba7f9869a8bb546c340ef5cc42d2859e71
Phan Nhat Hoang <johnphan1608@gmail.com>
2022-09-07 14:20:48 +0800

    changed the name of test cases

--- tests/cmaes_test.cpp
+++ tests/cmaes_test.cpp
@@ -88,75 +88,6 @@ void LogisticRegressionFunctionTestReal(OptimizerType& optimizer,
   }
 }
 
-template<typename MatType = arma::mat, typename OptimizerType>
-void LogisticRegressionFunctionTestRealM(OptimizerType& optimizer,
-                                        const std::string dataset_path,
-                                        const size_t dim = 200,
-                                        const size_t trials = 1,
-                                        const double split_ratio = 0.8)
-{
-  MatType data, testData, shuffledData;
-  arma::Row<size_t> responses, testResponses, shuffledResponses;
-
-  // Load the dataset
-  if (data.load(dataset_path, arma::csv_ascii) == false)
-  {
-    FAIL("couldn't load data");
-    return;
-  }
-  
-  data = data.t();
-  size_t n_row = data.n_rows;
-  responses = arma::conv_to<arma::Row<size_t>>::from(data.row(0));
-  data = data.rows(1, std::min(dim, n_row)-1);
-  arma::uvec indices = arma::shuffle(arma::linspace<arma::uvec>(0, 
-      data.n_cols - 1, data.n_cols)); 
-  size_t idx = data.n_cols * split_ratio;
-
-  shuffledData = MatType(data.n_rows, idx);
-  shuffledResponses = arma::Row<size_t>(idx);
-
-  testData = MatType(data.n_rows, data.n_cols-idx);
-  testResponses = arma::Row<size_t>(data.n_cols-idx);
-
-  for (size_t i = 0; i < idx; ++i)
-  {
-    shuffledData.col(i) = data.col(indices(i));
-    shuffledResponses.col(i) = responses.col(indices(i));
-  }
-  
-  for (size_t i = idx; i < data.n_cols; ++i)
-  {
-    testData.col(i-idx) = data.col(indices(i));
-    testResponses.col(i-idx) = responses.col(indices(i));
-  }
-
-  ens::test::LogisticRegression<MatType> lr(data, responses, 0.5);
-  arma::wall_clock clock;
-  double bestT = 0.0; double bestAcc = 0.0; double bestTestAcc = 0.0;
-  for (size_t i = 0; i < trials; ++i)
-  {
-    double lapsT = 0.0;
-    MatType coordinates = lr.GetInitialPoint();
-    clock.tic();
-    optimizer.Optimize(lr, coordinates);
-    lapsT += clock.toc();
-    const double acc = lr.ComputeAccuracy(shuffledData, shuffledResponses, 
-        coordinates);
-    const double testAcc = lr.ComputeAccuracy(testData, testResponses,
-        coordinates);
-
-    bestAcc = acc; bestTestAcc = testAcc; bestT = lapsT;    
-    // Provide a shortcut to try again if we're not on the last trial.
-    if (acc < bestAcc)
-      continue;
-    if (testAcc < bestTestAcc)
-      continue;
-    bestAcc = acc; bestTestAcc = testAcc; bestT = lapsT;    
-  }
-  std::cout << "following results: " << std::endl;
-  std::cout << bestAcc << " " << bestTestAcc << " " << bestT << std::endl;
-}
 /**
  * Run CMA-ES with the full selection policy on logistic regression and
  * make sure the results are acceptable.
@@ -270,37 +201,4 @@ TEST_CASE("VDCMAESTitanicTest", "[CMAESTest]")
 {
   VDCMAES cmaes(0, -1, 1, 32, 200, 1e-3);
   LogisticRegressionFunctionTestReal(cmaes, 0.003, 0.006, "data/titanic.csv", 5);
-}
-
-
-// Benchmarking purposes
-TEST_CASE("CMAESGisetteTest", "")
-{
-  CMAES<> cmaes(0, -1, 1, 32, 200, 1e-3);
-  LogisticRegressionFunctionTestRealM(cmaes, "data/gisette.csv", 5000, 1);
-}
-TEST_CASE("ApproxCMAESGisetteTest", "")
-{
-  ApproxCMAES cmaes(0, -1, 1, 32, 200, 1e-3);
-  LogisticRegressionFunctionTestRealM(cmaes, "data/gisette.csv", 5000, 1);
-}
-TEST_CASE("ActiveCMAESGisetteTest", "")
-{
-  ActiveCMAES cmaes(0, -1, 1, 32, 200, 1e-3);
-  LogisticRegressionFunctionTestRealM(cmaes, "data/gisette.csv", 5000, 1);
-}
-TEST_CASE("ActiveApproxCMAESGisetteTest", "")
-{
-  ActiveApproxCMAES cmaes(0, -1, 1, 32, 200, 1e-3);
-  LogisticRegressionFunctionTestRealM(cmaes, "data/gisette.csv", 5000, 1);
-}
-TEST_CASE("SepCMAESGisetteTest", "")
-{
-  SepCMAES cmaes(0, -1, 1, 32, 200, 1e-3);
-  LogisticRegressionFunctionTestRealM(cmaes, "data/gisette.csv", 5000, 1);
-}
-TEST_CASE("VDCMAESGisetteTest", "")
-{
-  VDCMAES cmaes(0, -1, 1, 32, 200, 1e-3);
-  LogisticRegressionFunctionTestRealM(cmaes, "data/gisette.csv", 5000, 1);
 }
