b4fb3d7f9e3fe3bb7ab1bd026e48c518d13f9278
IWNMWE <satyamtech42@gmail.com>
2024-07-24 22:29:10 +0530

    change file path

--- include/ensmallen.hpp
+++ include/ensmallen.hpp
@@ -112,7 +112,7 @@
 #include "ensmallen_bits/katyusha/katyusha.hpp"
 #include "ensmallen_bits/lbfgs/lbfgs.hpp"
 #include "ensmallen_bits/lookahead/lookahead.hpp"
-#include "ensmallen_bits/age_moea/agemoea.hpp"
+#include "ensmallen_bits/agemoea/agemoea.hpp"
 #include "ensmallen_bits/moead/moead.hpp"
 #include "ensmallen_bits/nsga2/nsga2.hpp"
 #include "ensmallen_bits/padam/padam.hpp"
