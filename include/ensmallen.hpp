53800b32b29972c81c6870e945648bdc28149967
Omar Shrit <omar@avontech.fr>
2025-04-02 11:19:08 +0200

    Remove proxies and follow the same strategy as in mlpack
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- include/ensmallen.hpp
+++ include/ensmallen.hpp
@@ -78,7 +78,6 @@
 #include "ensmallen_bits/utility/conv_to.hpp"
 #include "ensmallen_bits/utility/distr_param.hpp"
 #include "ensmallen_bits/utility/using.hpp"
-#include "ensmallen_bits/utility/proxies.hpp"
 #include "ensmallen_bits/utility/indicators/epsilon.hpp"
 #include "ensmallen_bits/utility/indicators/igd.hpp"
 #include "ensmallen_bits/utility/indicators/igd_plus.hpp"
